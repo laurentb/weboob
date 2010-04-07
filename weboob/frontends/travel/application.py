6e8d36ca937aa337914bfea9ca3b812e4bbea12a
Romain Bignon <romain@peerfuse.org>
2010-04-07 15:18:52 +0200

    unused imports

--- weboob/frontends/travel/application.py
+++ weboob/frontends/travel/application.py
@@ -18,9 +18,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 """
 
-import sys
-from types import MethodType
-
 from weboob.capabilities.travel import ICapTravel
 from weboob.tools.application import ConsoleApplication
 
