1bee1e5165552e6e18b36304c8a6299adfe20d02
Christophe Benz <christophe.benz@gmail.com>
2010-04-12 16:27:26 +0200

    minor fixes for monfric frontend

--- weboob/frontends/boobank/monfric.py
+++ weboob/frontends/boobank/monfric.py
@@ -1,7 +1,25 @@
-from weboob import Weboob
+# -*- coding: utf-8 -*-
+
+"""
+Copyright(C) 2010  Christophe Benz
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+"""
+
 from weboob.capabilities.bank import ICapBank
 from weboob.tools.application import BaseApplication
-from weboob.tools.config.yamlconfig import YamlConfig
 
 class MonFric(BaseApplication):
     APPNAME = 'monfric'
@@ -12,7 +30,7 @@ class MonFric(BaseApplication):
         requested_backend = self.config.get('backend')
         requested_account = self.config.get('account')
         if not requested_backend or not requested_account :
-            raise Exception(u'Please provide backend and account keys in config file "%s"' % self.config.path)
+            raise Exception(u'Please provide "backend" and "account" keys in config file "%s"' % self.config.path)
         self.weboob.load_backends(ICapBank)
         for account in self.weboob.backends[requested_backend].iter_accounts():
             if account.label == requested_account:
