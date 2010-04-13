41ef766e48393b7b8d0100346b21556cafc565b3
Christophe Benz <christophe.benz@gmail.com>
2010-04-13 16:46:13 +0200

    add html5lib parser

--- weboob/tools/parser/html5libparser.py
+++ weboob/tools/parser/html5libparser.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright(C) 2010  Christophe Benz
+Copyright(C) 2010  Romain Bignon, Christophe Benz
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -18,7 +18,15 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 """
 
-from .elementtidyparser import ElementTidyParser
-from .html5libparser import Html5libParser
-from .lxmlparser import LxmlHtmlParser
-from .standardparser import StandardParser
+from html5lib import treebuilders, HTMLParser
+try:
+    from xml.etree import cElementTree as ElementTree
+except ImportError:
+    from xml.etree import ElementTree
+
+class Html5libParser(HTMLParser):
+    def __init__(self, api='etree'):
+        HTMLParser.__init__(self, tree=treebuilders.getTreeBuilder(api, ElementTree))
+
+    def parse(self, data, encoding):
+        return HTMLParser.parse(self, data, encoding=encoding)
