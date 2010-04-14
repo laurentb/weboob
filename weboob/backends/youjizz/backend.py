ba32b120af69bd97ab2b3651761b361a1476a479
Roger Philibert <roger.philibert@gmail.com>
2010-04-15 01:32:34 +0200

    add backend youjizz

---
+++ weboob/backends/youjizz/backend.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright(C) 2010  Roger Philibert
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+"""
+
+from weboob.backend import Backend
+from weboob.capabilities.video import ICapVideoProvider
+
+from .browser import YoujizzBrowser
+
+class YoujizzBackend(Backend, ICapVideoProvider):
+    NAME = 'youjizz'
+    MAINTAINER = 'Roger Philibert'
+    EMAIL = 'roger.philibert@gmail.com'
+    VERSION = '0.1'
+    DESCRIPTION = 'Youjizz videos website'
+    LICENSE = 'GPLv3'
+
+    CONFIG = {}
+    browser = None
+
+    def need_browser(func):
+        def inner(self, *args, **kwargs):
+            if not self.browser:
+                self.browser = YoujizzBrowser()
+            url = args[0]
+            if u'youjizz.com' not in url:
+                return None
+            return func(self, *args, **kwargs)
+        return inner
+
+    @need_browser
+    def get_video_title(self, page_url):
+        return self.browser.get_video_title(page_url)
+
+    @need_browser
+    def get_video_url(self, page_url):
+        return self.browser.get_video_url(page_url)
+
+    @need_browser
+    def iter_page_urls(self, mozaic_url):
+        return self.browser.iter_page_urls(mozaic_url)
---
+++ weboob/backends/youjizz/browser.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright(C) 2010  Roger Philibert
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+"""
+
+from logging import error
+import re
+
+from weboob.tools.browser import Browser
+from weboob.tools.parser import LxmlHtmlParser
+
+class YoujizzBrowser(Browser):
+    video_file_regex = re.compile(r'"(http://media[^ ,]+\.flv)"')
+
+    def __init__(self, *args, **kwargs):
+        kwargs['parser'] = LxmlHtmlParser()
+        Browser.__init__(self, *args, **kwargs)
+
+    def iter_page_urls(self, mozaic_url):
+        raise NotImplementedError()
+
+    def get_video_title(self, page_url):
+        raise NotImplementedError()
+
+    def get_video_url(self, page_url):
+        data = self.openurl(page_url).read()
+        video_file_urls = re.findall(self.video_file_regex, data)
+        if len(video_file_urls) == 0:
+            return None
+        else:
+            if len(video_file_urls) > 1:
+                error('Many video file URL found for given URL: %s' % video_file_urls)
+            return video_file_urls[0]
+
