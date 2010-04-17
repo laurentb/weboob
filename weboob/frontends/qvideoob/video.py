74deb97d6a3c7f1b7b3e8670f8ed5b917c921a20
Romain Bignon <romain@peerfuse.org>
2010-04-17 12:21:55 +0200

    aibility to play video with phonon

--- weboob/frontends/qvideoob/video.py
+++ weboob/frontends/qvideoob/video.py
@@ -18,7 +18,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 """
 
+from PyQt4.QtCore  import QUrl
 from PyQt4.QtGui import QDialog
+from PyQt4.phonon import Phonon
 
 from weboob.frontends.qvideoob.ui.video_ui import Ui_Video
 
@@ -30,6 +32,7 @@ class Video(QDialog):
 
         self.video = video
         self.setWindowTitle("Video - %s" % video.title)
+        self.ui.urlEdit.setText(video.url)
         self.ui.titleLabel.setText(video.title)
         self.ui.durationLabel.setText('%d:%02d:%02d' % (video.duration/3600, (video.duration%3600)/60, video.duration%60))
         self.ui.authorLabel.setText(unicode(video.author))
@@ -38,3 +41,11 @@ class Video(QDialog):
             self.ui.ratingLabel.setText('%s / %s' % (video.rating, video.rating_max))
         else:
             self.ui.ratingLabel.setText('%s' % video.rating)
+
+        self.ui.seekSlider.setMediaObject(self.ui.videoPlayer.mediaObject())
+        self.ui.videoPlayer.load(Phonon.MediaSource(QUrl(video.url)))
+        self.ui.videoPlayer.play()
+
+    def closeEvent(self, event):
+        self.ui.videoPlayer.stop()
+        event.accept()
