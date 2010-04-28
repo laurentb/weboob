f7a92d9c727deb8c4839ced8cf0f654bdbe9cdf8
Christophe Benz <christophe.benz@gmail.com>
2010-04-28 14:48:46 +0200

    remove gdata hard dependency

--- setup.py
+++ setup.py
@@ -39,6 +39,5 @@ setup(
     packages=find_packages(exclude=['ez_setup']),
     scripts=[os.path.join('scripts', script) for script in os.listdir('scripts')],
     install_requires=[
-        'gdata',
     ]
 )
