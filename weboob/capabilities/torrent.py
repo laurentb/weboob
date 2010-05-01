245c44987d9ccff9966047f9fbfa436df4db7d3c
Romain Bignon <romain@peerfuse.org>
2010-05-01 14:37:28 +0200

    new ICapTorrent capability

---
+++ weboob/capabilities/torrent.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright(C) 2010  Romain Bignon
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+"""
+
+from .cap import ICap
+
+
+__all__ = ['ICapTorrent']
+
+class Torrent(object):
+    def __init__(self, id, name, date=None, size=0.0, url=u'', seeders=0, leechers=0, files=[])
+        self.id = id
+        self.name = name
+        self.date = date
+        self.size = size
+        self.ul = url
+        self.seeders = seeders
+        self.leechers = leechers
+        self.files = files
+
+class ICapTorrent(ICap):
+    def iter_torrents(self, pattern):
+        raise NotImplementedError()
+
+    def get_torrent(self, _id):
+        raise NotImplementedError()
