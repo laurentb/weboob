056dec231d6515e578330ce935dbc5a6ef6bd5f9
Christophe Benz <christophe.benz@gmail.com>
2010-05-01 18:56:50 +0200

    add __future__ import

--- weboob/frontends/weboorrents/application.py
+++ weboob/frontends/weboorrents/application.py
@@ -18,6 +18,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 """
 
+from __future__ import with_statement
+
 import sys
 
 from weboob.capabilities.torrent import ICapTorrent
