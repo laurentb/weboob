bd833e189f1c615a36afbfec4522f1a0665db5be
Romain Bignon <romain@peerfuse.org>
2010-05-03 21:03:20 +0200

    fix import

--- weboob/backends/dlfp/backend.py
+++ weboob/backends/dlfp/backend.py
@@ -18,7 +18,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 """
 
-from weboob.backend import BaseBackend, BrowserUnavailable
+from weboob.backend import BaseBackend
+from weboob.tools.browser import BrowserUnavailable
 from weboob.capabilities.messages import ICapMessages, ICapMessagesReply, Message
 
 from .feeds import ArticlesList
