3f938a957b88ad7feb62b84e2cdca01d7f496f9f
Romain Bignon <romain@peerfuse.org>
2010-05-17 11:24:46 +0200

    fix: function was called twice each times

--- weboob/scheduler.py
+++ weboob/scheduler.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
 # Copyright(C) 2010  Romain Bignon, Christophe Benz
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 3 of the License.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
@@ -62,5 +62,4 @@ class Scheduler(IScheduler):
         self.stop_event.set()
 
     def _repeated_cb(self, interval, function, args):
-        function(*args)
         self.repeat(interval, function, *args)
