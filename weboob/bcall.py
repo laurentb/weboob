7a51e53faa8978acf77deece8c7818b12c4c9fef
Romain Bignon <romain@peerfuse.org>
2010-05-28 13:50:44 +0200

    fix CallErrors constructor (refs #290)

--- weboob/bcall.py
+++ weboob/bcall.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
 # Copyright(C) 2010  Romain Bignon, Christophe Benz
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 3 of the License.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
@@ -31,7 +31,7 @@ class CallErrors(Exception):
         Exception.__init__(self, u'These errors have been raised in backend threads:\n%s' % (
             u'\n'.join((u' * %s: %s%s' % (backend, error, backtrace + '\n'
                                           if logging.root.level == logging.DEBUG else ''))
-                       for backend, error, backtrace in self.errors)))
+                       for backend, error, backtrace in errors)))
         self.errors = copy(errors)
 
     def __iter__(self):
