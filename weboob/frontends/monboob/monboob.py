d8824c99d62efc46e09f30bd54cc568fb1de3740
Romain Bignon <romain@peerfuse.org>
2010-06-09 13:09:05 +0200

    Revert "new option 'backends' to set a restricted list of backends to load"
    This reverts commit 110a975735b21b117ba7676b92d292366bc5ef31.

--- weboob/frontends/monboob/monboob.py
+++ weboob/frontends/monboob/monboob.py
@@ -42,17 +42,11 @@ class Monboob(ConsoleApplication):
               'domain':    'weboob.example.org',
               'recipient': 'weboob@example.org',
               'smtp':      'localhost',
-              'backends':  '',
               'html':      0}
 
     def main(self, argv):
         self.load_config()
-
-        names = None
-        if self.config.get('backends'):
-            names = self.config.get('backends').split()
-
-        self.load_backends(ICapMessages, names=names, storage=self.create_storage())
+        self.load_backends(ICapMessages, storage=self.create_storage())
 
         return self.process_command(*argv[1:])
 
