a9e1b627a90bf0de9911f2f4664f3042ac1e10c7
Christophe Benz <christophe.benz@gmail.com>
2010-07-07 18:08:02 +0200

    rename setup scripts in all_packages.sh

--- tools/all_packages.sh
+++ tools/all_packages.sh
@@ -5,30 +5,31 @@ SCRIPT_DIRNAME=$(dirname $0)
 COMMAND="$1"
 
 SETUP_PY_LIST="
-$SCRIPT_DIRNAME/setup.py.d/setup-core.py
+$SCRIPT_DIRNAME/setup.py.d/core.py
+$SCRIPT_DIRNAME/setup.py.d/core-qt.py
 
-$SCRIPT_DIRNAME/setup.py.d/setup-bank-backends.py
-$SCRIPT_DIRNAME/setup.py.d/setup-dating-backends.py
-$SCRIPT_DIRNAME/setup.py.d/setup-messages-backends.py
-$SCRIPT_DIRNAME/setup.py.d/setup-torrent-backends.py
-$SCRIPT_DIRNAME/setup.py.d/setup-travel-backends.py
-$SCRIPT_DIRNAME/setup.py.d/setup-video-backends.py
-$SCRIPT_DIRNAME/setup.py.d/setup-video-nsfw-backends.py
-$SCRIPT_DIRNAME/setup.py.d/setup-weather-backends.py
+$SCRIPT_DIRNAME/setup.py.d/backends-bank.py
+$SCRIPT_DIRNAME/setup.py.d/backends-dating.py
+$SCRIPT_DIRNAME/setup.py.d/backends-messages.py
+$SCRIPT_DIRNAME/setup.py.d/backends-torrent.py
+$SCRIPT_DIRNAME/setup.py.d/backends-travel.py
+$SCRIPT_DIRNAME/setup.py.d/backends-video.py
+$SCRIPT_DIRNAME/setup.py.d/backends-video-nsfw.py
+$SCRIPT_DIRNAME/setup.py.d/backends-weather.py
 
-$SCRIPT_DIRNAME/setup.py.d/setup-boobank.py
-$SCRIPT_DIRNAME/setup.py.d/setup-havesex.py
-$SCRIPT_DIRNAME/setup.py.d/setup-masstransit.py
-$SCRIPT_DIRNAME/setup.py.d/setup-monboob.py
-$SCRIPT_DIRNAME/setup.py.d/setup-qboobmsg.py
-$SCRIPT_DIRNAME/setup.py.d/setup-qhavesex.py
-$SCRIPT_DIRNAME/setup.py.d/setup-qvideoob.py
-$SCRIPT_DIRNAME/setup.py.d/setup-qweboobcfg.py
-$SCRIPT_DIRNAME/setup.py.d/setup-travel.py
-$SCRIPT_DIRNAME/setup.py.d/setup-videoob.py
-$SCRIPT_DIRNAME/setup.py.d/setup-videoob_web.py
-$SCRIPT_DIRNAME/setup.py.d/setup-weboorrents.py
-$SCRIPT_DIRNAME/setup.py.d/setup-wetboobs.py
+$SCRIPT_DIRNAME/setup.py.d/boobank.py
+$SCRIPT_DIRNAME/setup.py.d/havesex.py
+$SCRIPT_DIRNAME/setup.py.d/masstransit.py
+$SCRIPT_DIRNAME/setup.py.d/monboob.py
+$SCRIPT_DIRNAME/setup.py.d/qboobmsg.py
+$SCRIPT_DIRNAME/setup.py.d/qhavesex.py
+$SCRIPT_DIRNAME/setup.py.d/qvideoob.py
+$SCRIPT_DIRNAME/setup.py.d/qweboobcfg.py
+$SCRIPT_DIRNAME/setup.py.d/travel.py
+$SCRIPT_DIRNAME/setup.py.d/videoob.py
+$SCRIPT_DIRNAME/setup.py.d/videoob_web.py
+$SCRIPT_DIRNAME/setup.py.d/weboorrents.py
+$SCRIPT_DIRNAME/setup.py.d/wetboobs.py
 "
 
 for f in $SETUP_PY_LIST
