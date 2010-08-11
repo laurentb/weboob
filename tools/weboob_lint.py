0202cf2f7d6ebc39355a5bf7c94ca0e290b47a8e
Christophe Benz <christophe.benz@gmail.com>
2010-08-11 19:27:52 +0200

    add weboob_lint.py script

---
+++ tools/weboob_lint.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from weboob.core.backends import BackendsLoader
+
+loader = BackendsLoader()
+loader.load_all()
+
+backends_without_icons = [name for name, backend in loader.loaded.iteritems() if backend.icon_path is None]
+if backends_without_icons:
+    print 'Backends without icons: %s' % backends_without_icons
