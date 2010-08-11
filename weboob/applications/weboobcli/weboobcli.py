a7d2aa21e85ff7147558d2c16fbd104d08966237
Romain Bignon <romain@peerfuse.org>
2010-08-11 14:59:10 +0200

    new backend weboob-cli (refs #352)

---
+++ weboob/applications/weboobcli/weboobcli.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2010  Romain Bignon
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+import sys
+
+from weboob.tools.application.console import ConsoleApplication
+
+
+__all__ = ['WeboobCli']
+
+
+class WeboobCli(ConsoleApplication):
+    APPNAME = 'weboob-cli'
+    VERSION = '0.1'
+    COPYRIGHT = 'Copyright(C) 2010 Romain Bignon'
+
+    def main(self, argv):
+        if len(argv) < 3:
+            print >>sys.stderr, "Syntax: %s capability command [args ..]" % argv[0]
+            return 1
+
+        cap_s = argv[1]
+        cmd = argv[2]
+        args = argv[3:]
+
+        self.load_backends(cap_s)
+
+        for backend, obj in self.weboob.do(cmd, *args):
+            self.format(obj, backend.name)
+
+        return 0
