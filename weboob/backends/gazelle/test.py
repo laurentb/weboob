c3805a8f236bed1ceb8cb4370d99a48ab12293c8
Romain Bignon <romain@peerfuse.org>
2010-08-13 23:58:14 +0200

    gazelle tests

---
+++ weboob/backends/gazelle/test.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2010  Romain Bignon
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+from weboob.tools.test import BackendTest
+
+class GazelleTest(BackendTest):
+    BACKEND = 'gazelle'
+
+    def test_torrent(self):
+        l = [torrent for torrent in self.backend.iter_torrents('sex')]
+        if len(l) > 0:
+            f = self.backend.get_torrent_file(l[0].id)
