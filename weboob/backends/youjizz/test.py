9e2b399472fcc1b684d92767507f0e92a6a5e62e
Romain Bignon <romain@peerfuse.org>
2010-08-14 00:27:44 +0200

    youtube youporn and youjizz tests

---
+++ weboob/backends/youjizz/test.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2010  Romain Bignon
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+from weboob.tools.test import BackendTest
+
+class YoujizzTest(BackendTest):
+    BACKEND = 'youjizz'
+
+    def test_youjizz(self):
+        self.assertTrue(len(self.backend.iter_search_results('anus', nsfw=False)) == 0)
+
+        l = [v for v in self.backend.iter_search_results('futanari', nsfw=True)]
+        self.assertTrue(len(l) > 0)
+        v = l[0]
+        self.backend.fillobj(v, ('url',))
+        self.assertTrue(v.url and v.url.startswith('http://'), 'URL for video "%s" not found: %s' % (v.id, v.url))
