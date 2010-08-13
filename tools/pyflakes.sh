28f622e0717f5bee43e8f4e96f099559acd206e2
Romain Bignon <romain@peerfuse.org>
2010-08-13 22:22:42 +0200

    fix script to grep -v only one message type (redefinition)

--- tools/pyflakes.sh
+++ tools/pyflakes.sh
@@ -2,4 +2,7 @@
 if [ `basename $PWD` == 'tools' ]; then
         cd ..
 fi
-pyflakes weboob scripts/* | grep -v __init__.py
+
+# grep will return 0 only if it founds something, but our script
+# wants to return 0 when it founds nothing!
+pyflakes weboob scripts/* | grep -v redefinition && exit 1 || exit 0
