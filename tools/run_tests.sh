876d05ffa6378b29c37934361cf5a97c06191655
Romain Bignon <romain@peerfuse.org>
2010-08-30 16:58:56 +0200

    when there is a param, load test only for this backend

--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-find $(dirname $0)/../weboob -name test.py | xargs nosetests -sv
+if [ "$1" != "" ]; then
+	nosetests -sv $(dirname $0)/../weboob/backends/$1
+else
+	find $(dirname $0)/../weboob -name test.py | xargs nosetests -sv
+fi
