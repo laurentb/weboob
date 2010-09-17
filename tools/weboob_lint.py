0dc44850767acfc512bd6ee6645add7cf10247d9
Romain Bignon <romain@peerfuse.org>
2010-09-17 19:09:46 -0400

    fix script

--- tools/weboob_lint.py
+++ tools/weboob_lint.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-from weboob.core.backends import BackendsLoader
+from weboob.core.modules import ModulesLoader
 
-loader = BackendsLoader()
+loader = ModulesLoader()
 loader.load_all()
 
 backends_without_icons = [name for name, backend in loader.loaded.iteritems() if backend.icon_path is None]
