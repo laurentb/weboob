af8842bb0bdb4f550a312cc66c555c59ce01252d
nicolas <nicolas@NicolasDesktop.(none)>
2010-09-23 22:15:16 +0200

    Committer: nicolas <nicolas@NicolasDesktop.(none)>
    New backend, bp, La Banque Postale
    Implante
     - get_accounts_list
     - get_history
    
    	new file:   weboob/backends/bp/__init__.py
    	new file:   weboob/backends/bp/backend.py
    	new file:   weboob/backends/bp/browser.py
    
    Signed-off-by: Nicolas Duhamel <nicolas@jombi.fr>
    Signed-off-by: Romain Bignon <romain@peerfuse.org>

---
+++ weboob/backends/bp/backend.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+#
+#	   backend.py
+#	   
+#	   Copyright 2010 nicolas <nicolas@NicolasDesktop>
+#	   
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+from weboob.capabilities.bank import ICapBank, AccountNotFound
+from weboob.tools.backend import BaseBackend
+
+from .browser import BPbrowser
+
+class BPBackend(BaseBackend, ICapBank):
+	NAME = 'bp'
+	MAINTAINER = 'Nicolas Duhamel'
+	EMAIL = 'nicolas@jombi.fr'
+	VERSION = '0.1'
+	LICENSE = 'GPLv3'
+	DESCRIPTION = 'La banque postale, banque française'
+	CONFIG = {'login':	BaseBackend.ConfigField(description='Account ID'),
+			  'password': BaseBackend.ConfigField(description='Password of account', is_masked=True)
+			 }
+	BROWSER = BPbrowser
+	
+	def create_default_browser(self):
+		return self.create_browser(self.config['login'], self.config['password'])
+
+	def iter_accounts(self):
+		for account in self.browser.get_accounts_list():
+			yield account
+			
+	def get_account(self, _id):
+		account = self.browser.get_account(_id)
+		if account:
+			return account
+		else:
+			raise AccountNotFound()
+	
+	def iter_history(self, account):
+		for history in self.browser.get_history(account):
+			yield history
+	
