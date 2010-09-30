0cdcd10b52247dbf5a72d34ae7ec29236a0b560d
Laurent Bachelier <laurent@bachelier.name>
2010-09-30 23:49:15 +0200

    Fix syntax error with Python<2.6

--- weboob/tools/browser/browser.py
+++ weboob/tools/browser/browser.py
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-
+from __future__ import with_statement
 import mechanize
 import urllib
 import urllib2
