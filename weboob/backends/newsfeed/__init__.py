3f78167be50df4b11416fab3885d2fbdb3959804
Clément Schreiner <clemux@clemux.info>
2010-10-13 23:30:26 +0200

    Add missing copyright comments

--- weboob/backends/newsfeed/__init__.py
+++ weboob/backends/newsfeed/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright(C) 2010  Romain Bignon
+# Copyright(C) 2010  Clément Schreiner
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
