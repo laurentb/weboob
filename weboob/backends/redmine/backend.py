3d9f669d933e5495b4ef087768134dc9361d5b1f
Romain Bignon <romain@peerfuse.org>
2010-10-15 16:50:32 +0200

    python < 2.6 compatibility

--- weboob/backends/redmine/backend.py
+++ weboob/backends/redmine/backend.py
@@ -16,6 +16,8 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 
+from __future__ import with_statement
+
 from weboob.capabilities.content import ICapContent, Content
 from weboob.tools.backend import BaseBackend
 
