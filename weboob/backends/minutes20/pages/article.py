56a7b4fbaf9b1373b43202318e7f0c99cee43832
Juke <juke@free.fr>
2011-02-03 01:45:35 +0100

    remove unused import

--- weboob/backends/minutes20/pages/article.py
+++ weboob/backends/minutes20/pages/article.py
@@ -16,7 +16,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 from weboob.tools.browser import BasePage
-from weboob.tools.parsers.lxmlparser import select, SelectElementException
+from weboob.tools.parsers.lxmlparser import select
 
 class Article(object):
     def __init__(self):
