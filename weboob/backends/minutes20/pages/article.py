cc5e768ab20814da6ac96a64fcb207aa519cd4ae
Juke <juke@free.fr>
2011-02-08 01:48:11 +0100

    line too long

--- weboob/backends/minutes20/pages/article.py
+++ weboob/backends/minutes20/pages/article.py
@@ -21,14 +21,15 @@ from .minutes20 import Minutes20Page, NoAuthorElement
 
 class ArticlePage(Minutes20Page):
     def set_body(self):
-        self.element_body = select(self.main_div, "div.mna-body", 1) 
+        self.element_body = select(self.main_div, "div.mna-body", 1)
         self.element_body.remove(select(self.element_body, "div.mna-tools", 1))
         try:
-            self.element_body.remove(select(self.element_body, "div.mna-comment-call", 1))
+            self.element_body.remove(
+                select(self.element_body, "div.mna-comment-call", 1))
         except SelectElementException:
             pass
         try:
             self.element_body.remove(self.get_element_author())
         except NoAuthorElement:
             pass
-        self.article.body = self.browser.parser.tostring(self.element_body) 
+        self.article.body = self.browser.parser.tostring(self.element_body)
