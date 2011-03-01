a33c15e2e361a973c59b99e57173b68d23e92213
Romain Bignon <romain@peerfuse.org>
2011-03-01 08:20:41 +0100

    fix when uri is empty (closes #488)

--- weboob/tools/browser/browser.py
+++ weboob/tools/browser/browser.py
@@ -245,7 +245,7 @@ class BaseBrowser(mechanize.Browser):
 
     def check_location(func):
         def inner(self, *args, **kwargs):
-            if args and isinstance(args[0], basestring) and args[0][0] == '/' and \
+            if args and isinstance(args[0], basestring) and args[0].startswith('/') and \
                (not self.request or self.request.host != self.DOMAIN):
                 args = ('%s://%s%s' % (self.PROTOCOL, self.DOMAIN, args[0]),) + args[1:]
 
