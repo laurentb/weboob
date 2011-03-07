6d1124018e83cbad2b229b9a72af4065d2d5964c
Romain Bignon <romain@symlink.me>
2011-03-07 17:48:23 +0100

    unused import

--- weboob/backends/dlfp/pages/board.py
+++ weboob/backends/dlfp/pages/board.py
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-from weboob.tools.parsers.lxmlparser import select, SelectElementException
+from weboob.tools.parsers.lxmlparser import select
 from weboob.tools.browser import BasePage
 
 class Message(object):
