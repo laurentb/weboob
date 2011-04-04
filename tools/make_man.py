d3acf49d877ab68bb8099ed55d0bdb6013aec3c2
Christophe Benz <christophe.benz@gmail.com>
2011-04-04 14:29:31 +0200

    python2.5 compatibility.

--- tools/make_man.py
+++ tools/make_man.py
@@ -16,6 +16,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
+from __future__ import with_statement
+
 import sys, os, tempfile
 import imp, inspect
 import optparse
