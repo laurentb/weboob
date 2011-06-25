73ec029b4386050cb50ff99de976bd8fef6bae2b
Romain Bignon <romain@peerfuse.org>
2011-06-25 12:58:23 +0200

    new backend pluzz (closes #426)

---
+++ weboob/backends/pluzz/backend.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2011  Romain Bignon
+#
+# This file is part of weboob.
+#
+# weboob is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# weboob is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with weboob. If not, see <http://www.gnu.org/licenses/>.
+
+
+from __future__ import with_statement
+
+from weboob.capabilities.video import ICapVideo
+from weboob.tools.backend import BaseBackend
+
+from .browser import PluzzBrowser
+from .video import PluzzVideo
+
+
+__all__ = ['PluzzBackend']
+
+
+class PluzzBackend(BaseBackend, ICapVideo):
+    NAME = 'pluzz'
+    MAINTAINER = 'Romain Bignon'
+    EMAIL = 'romain@weboob.org'
+    VERSION = '0.9'
+    DESCRIPTION = 'France Television video website'
+    LICENSE = 'AGPLv3+'
+    BROWSER = PluzzBrowser
+
+    def get_video(self, _id):
+        with self.browser:
+            return self.browser.get_video(_id)
+
+    def iter_search_results(self, pattern=None, sortby=ICapVideo.SEARCH_RELEVANCE, nsfw=False, max_results=None):
+        with self.browser:
+            return self.browser.iter_search_results(pattern)
+
+    def fill_video(self, video, fields):
+        if fields != ['thumbnail']:
+            # if we don't want only the thumbnail, we probably want also every fields
+            with self.browser:
+                video = self.browser.get_video(PluzzVideo.id2url(video.id), video)
+        if 'thumbnail' in fields and video.thumbnail:
+            with self.browser:
+                video.thumbnail.data = self.browser.readurl(video.thumbnail.url)
+
+        return video
+
+    OBJECTS = {PluzzVideo: fill_video}
---
+++ weboob/backends/pluzz/browser.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2011  Romain Bignon
+#
+# This file is part of weboob.
+#
+# weboob is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# weboob is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with weboob. If not, see <http://www.gnu.org/licenses/>.
+
+from urllib import quote_plus
+
+from weboob.tools.browser import BaseBrowser
+from weboob.tools.browser.decorators import id2url
+
+from .pages import IndexPage, VideoPage, MetaVideoPage
+from .video import PluzzVideo
+
+
+__all__ = ['PluzzBrowser']
+
+
+class PluzzBrowser(BaseBrowser):
+    DOMAIN = 'pluzz.fr'
+    ENCODING = None
+    PAGES = {r'http://[w\.]*pluzz.fr/?': IndexPage,
+             r'http://[w\.]*pluzz.fr/recherche.html.*': IndexPage,
+             r'http://[w\.]*pluzz.fr/[-\w]+/.*': IndexPage,
+             r'http://[w\.]*pluzz.fr/((?!recherche).+)\.html': VideoPage,
+             r'http://info\.francetelevisions\.fr/\?id-video=.*': MetaVideoPage,
+            }
+
+    @id2url(PluzzVideo.id2url)
+    def get_video(self, url, video=None):
+        self.location(url)
+        assert self.is_on_page(VideoPage)
+
+        metaurl = self.page.get_meta_url()
+        id = self.page.get_id()
+        self.location(metaurl)
+        assert self.is_on_page(MetaVideoPage)
+
+        return self.page.get_video(id, video)
+
+    def iter_search_results(self, pattern):
+        if not pattern:
+            self.home()
+        else:
+            self.location(self.buildurl('recherche.html', q=pattern))
+
+        assert self.is_on_page(IndexPage)
+        return self.page.iter_videos()
