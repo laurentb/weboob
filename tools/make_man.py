6b8a1631586079fa167e926f88ec637dae8b6a09
Romain Bignon <romain@peerfuse.org>
2012-03-03 23:19:38 +0100

    change paths of related files to XDG specs

--- tools/make_man.py
+++ tools/make_man.py
@@ -164,9 +164,9 @@ For full COPYRIGHT see COPYING file with weboob package.
 .LP
 .RE
 .SH FILES
- "~/.weboob/backends" """ % application.COPYRIGHT
+ "~/.config/weboob/backends" """ % application.COPYRIGHT
     if len(app.CONFIG) > 0:
-        footer += '\n\n "~/.weboob/%s"' % app.APPNAME
+        footer += '\n\n "~/.config/weboob/%s"' % app.APPNAME
 
     # Skip internal applications.
     footer += "\n\n.SH SEE ALSO\nHome page: http://weboob.org/applications/%s" % application.APPNAME
