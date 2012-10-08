6bcd6d21fd0ae9712fa18266e9e58b89ad7b266c
Romain Bignon <romain@symlink.me>
2012-10-08 08:27:33 +0200

    bnp certificate changed

--- modules/bnporc/browser.py
+++ modules/bnporc/browser.py
@@ -37,7 +37,7 @@ __all__ = ['BNPorc']
 class BNPorc(BaseBrowser):
     DOMAIN = 'www.secure.bnpparibas.net'
     PROTOCOL = 'https'
-    CERTHASH = '5511f0ff19c982b6351c17b901bfa7419f075edb13f2df41e446248beb7866bb'
+    CERTHASH = 'f7dae8d9e0f2c63a1c35d8cf1a86fdd5441a897340538b045ba8197f40c28cad'
     ENCODING = None  # refer to the HTML encoding
     PAGES = {'.*pageId=unedescomptes.*':                    AccountsList,
              '.*pageId=releveoperations.*':                 AccountHistory,
