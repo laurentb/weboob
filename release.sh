3e2109902dfdbe418fde097b0eab1f5d609be038
Romain Bignon <romain@symlink.me>
2012-10-28 19:47:31 +0100

    update release script

--- release.sh
+++ release.sh
@@ -2,9 +2,9 @@
 # This script is used to release a version.
 
 function set_version {
-	echo -n "Replacing version in source files"
+	echo -n "Replacing version in source files to $1"
 	for fl in $(find . -iname "*.py"); do
-		sed "s/\(VERSION\|version\|release\) *= *[\"'][0-9]\+\..\+[\"'],?$/\1 = '$1'/g" $fl > $fl.new
+		sed "s/\(VERSION\|version\|release\) *= *[\"'][0-9]\+\..\+[\"'],\?$/\1 = '$1'/g" $fl > $fl.new
 		diff $fl.new $fl >/dev/null && echo -n "." || echo -n "+"
 		cp -f $fl.new $fl
 		rm -f $fl.new
