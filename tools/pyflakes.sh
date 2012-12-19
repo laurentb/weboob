c64fb9150e7ca0310637f8dd716e808af1997e6f
Laurent Bachelier <laurent@bachelier.name>
2012-12-19 15:12:18 +0100

    Support systems with flake8 for python3 by default

--- tools/pyflakes.sh
+++ tools/pyflakes.sh
@@ -2,9 +2,17 @@
 cd $(dirname $0)
 cd ..
 
+FLAKE8=""
 if which flake8 >/dev/null 2>&1; then
+    FLAKE8=flake8
+fi
+if which flake8-python2 >/dev/null 2>&1; then
+    FLAKE8=flake8-python2
+fi
+
+if [ -n "${FLAKE8}" ]; then
     set -e
-    flake8 --ignore=E,W *.py weboob modules contrib scripts/*
+    ${FLAKE8} --ignore=E,W *.py weboob modules contrib scripts/*
 else
     # grep will return 0 only if it founds something, but our script
     # wants to return 0 when it founds nothing!
