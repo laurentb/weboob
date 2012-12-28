2246f893bcc059c02acf0d526654962d071edb7e
Romain Bignon <romain@symlink.me>
2012-12-28 17:48:52 +0100

    add module for Credit Mutuel Sud Ouest (cmso)

---
+++ modules/cmso/browser.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2012 Romain Bignon
+#
+# This file is part of weboob.
+#
+# weboob is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# weboob is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with weboob. If not, see <http://www.gnu.org/licenses/>.
+
+
+from weboob.tools.browser import BaseBrowser, BrowserIncorrectPassword
+
+from .pages import LoginPage, AccountsPage, TransactionsPage
+
+
+__all__ = ['Cmso']
+
+
+class Cmso(BaseBrowser):
+    PROTOCOL = 'https'
+    DOMAIN = 'www.cmso.com'
+    PAGES = {'https://www.cmso.com/domimobile/m.jsp\?a=signin.*':       LoginPage,
+             'https://www.cmso.com/domimobile/m.jsp\?a=sommaire.*':     AccountsPage,
+             'https://www.cmso.com/domimobile/m.jsp\?a=solde.*':        TransactionsPage,
+             'https://www.cmso.com/domimobile/m.jsp\?rels=.*':          TransactionsPage,
+            }
+
+    def is_logged(self):
+        return self.page is not None and not self.is_on_page(LoginPage)
+
+    def home(self):
+        if self.is_logged():
+            self.location('https://www.cmso.com/domimobile/m.jsp?a=sommaire')
+        else:
+            self.login()
+
+    def login(self):
+        """
+        Attempt to log in.
+        Note: this method does nothing if we are already logged in.
+        """
+        assert isinstance(self.username, basestring)
+        assert isinstance(self.password, basestring)
+
+        if self.is_logged():
+            return
+
+        if not self.is_on_page(LoginPage):
+            self.location('https://www.cmso.com/domimobile/m.jsp?a=signin&b=sommaire', no_login=True)
+
+        self.page.login(self.username, self.password)
+
+        if not self.is_logged():
+            raise BrowserIncorrectPassword()
+
+    def get_accounts_list(self):
+        if not self.is_on_page(AccountsPage):
+            self.location('https://www.cmso.com/domimobile/m.jsp?a=sommaire')
+        return self.page.get_list()
+
+    def get_account(self, id):
+        assert isinstance(id, basestring)
+
+        l = self.get_accounts_list()
+        for a in l:
+            if a.id == id:
+                return a
+
+        return None
+
+    def get_history(self, account):
+        link = account._link
+
+        while link is not None:
+            self.location(link)
+            assert self.is_on_page(TransactionsPage)
+
+            for tr in self.page.get_history():
+                yield tr
+
+            link = self.page.get_next_link()
---
+++ modules/cmso/pages.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2012 Romain Bignon
+#
+# This file is part of weboob.
+#
+# weboob is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# weboob is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with weboob. If not, see <http://www.gnu.org/licenses/>.
+
+
+import datetime
+from decimal import Decimal
+import re
+
+from weboob.tools.browser import BasePage
+from weboob.capabilities.bank import Account
+from weboob.tools.capabilities.bank.transactions import FrenchTransaction
+
+
+__all__ = ['LoginPage', 'AccountsPage', 'TransactionsPage']
+
+
+class LoginPage(BasePage):
+    def login(self, login, passwd):
+        self.browser.select_form(name='formIdentification')
+        self.browser['noPersonne'] = login.encode(self.browser.ENCODING)
+        self.browser['motDePasse'] = passwd.encode(self.browser.ENCODING)
+        self.browser.submit(nologin=True)
+
+class AccountsPage(BasePage):
+    def get_list(self):
+        for li in self.document.xpath('//div[@class="affichMontant"]/ul/li/a'):
+            account = Account()
+            account.id = li.attrib['accesskey']
+            account.label = unicode(li.cssselect('div.row-lib u')[0].text.strip())
+            account.balance = Decimal(li.cssselect('p.row-right')[0].text.strip().replace(' ', '').replace(',', ''))
+            account._link = li.attrib['href']
+            yield account
+
+
+class Transaction(FrenchTransaction):
+    PATTERNS = [(re.compile('^RET DAB (?P<dd>\d{2})/(?P<mm>\d{2})/(?P<yy>\d{2}) (?P<text>.*)'),
+                                                              FrenchTransaction.TYPE_WITHDRAWAL),
+                (re.compile('CARTE (?P<dd>\d{2})/(?P<mm>\d{2}) (?P<text>.*)'),
+                                                              FrenchTransaction.TYPE_CARD),
+                (re.compile('^(?P<category>VIR(EMEN)?T? (SEPA)?(RECU|FAVEUR)?)( /FRM)?(?P<text>.*)'),
+                                                              FrenchTransaction.TYPE_TRANSFER),
+                (re.compile('^PRLV (?P<text>.*)( \d+)?$'),    FrenchTransaction.TYPE_ORDER),
+                (re.compile('^(CHQ|CHEQUE) .*$'),             FrenchTransaction.TYPE_CHECK),
+                (re.compile('^(AGIOS /|FRAIS) (?P<text>.*)'), FrenchTransaction.TYPE_BANK),
+                (re.compile('^(CONVENTION \d+ |F )?COTIS(ATION)? (?P<text>.*)'),
+                                                              FrenchTransaction.TYPE_BANK),
+                (re.compile('^REMISE (?P<text>.*)'),          FrenchTransaction.TYPE_DEPOSIT),
+                (re.compile('^(?P<text>.*)( \d+)? QUITTANCE .*'),
+                                                              FrenchTransaction.TYPE_ORDER),
+                (re.compile('^.* LE (?P<dd>\d{2})/(?P<mm>\d{2})/(?P<yy>\d{2})$'),
+                                                              FrenchTransaction.TYPE_UNKNOWN),
+               ]
+
+
+class TransactionsPage(BasePage):
+    months = [u'janvier', u'février', u'mars', u'avril', u'mai', u'juin', u'juillet', u'août', u'septembre', u'octobre', u'novembre', u'décembre']
+
+    def get_next_link(self):
+        a = self.document.getroot().cssselect('div#nav-sub p.row-right a')
+        if len(a) == 0:
+            return None
+
+        return a[0].attrib['href']
+
+    def get_history(self):
+        for div in self.document.xpath('//ol[@class="affichMontant"]/li/div'):
+            t = Transaction(0)
+            raw = div.xpath('.//div[@class="row-lib"]')[0].text
+            date = div.xpath('.//span')[0].text.strip()
+            m = re.match('(\d+) ([^ ]+)( \d+)?$', date)
+            if m:
+                dd = int(m.group(1))
+                mm = self.months.index(m.group(2)) + 1
+                if m.group(3) is not None:
+                    yy = int(m.group(3))
+                else:
+
+                    d = datetime.date.today()
+                    try:
+                        d = d.replace(month=mm, day=dd)
+                    except ValueError:
+                        d = d.replace(year=d.year-1, month=mm, day=dd)
+
+                    yy = d.year
+                    if d > datetime.date.today():
+                        yy -= 1
+
+                date = datetime.date(yy, mm, dd)
+
+            t.parse(date, re.sub(r'[ ]+', ' ', raw))
+            t.amount = Decimal(div.xpath('.//span')[-1].text.strip().replace(' ', '').replace(',', ''))
+
+            yield t
