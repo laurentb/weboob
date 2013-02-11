757070e7cdfa153e42c835c33709471f579a4c45
Florent <weboob@flo.fourcot.fr>
2013-02-11 14:03:42 +0100

    Increase RSS size
    The RSS feed contains one month of articles. Since the split
    of today news in a lot of entries, the size of the feed is
    very large.

--- modules/presseurop/backend.py
+++ modules/presseurop/backend.py
@@ -38,7 +38,7 @@ class NewspaperPresseuropBackend(GenericNewspaperBackend, ICapMessages):
     BROWSER = NewspaperPresseuropBrowser
     RSSID = staticmethod(rssid)
     URL2ID = staticmethod(url2id)
-    RSSSIZE = 180
+    RSSSIZE = 300
     CONFIG = BackendConfig(Value('lang', label='Lang of articles',
                            choices={'fr': 'fr', 'de': 'de', 'en': 'en',
                                 'cs': 'cs', 'es': 'es', 'it': 'it', 'nl': 'nl',
