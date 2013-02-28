af3c64549ea204975b0eac94d962abbd570ceff5
Florent <weboob@flo.fourcot.fr>
2013-02-28 15:08:08 +0100

    No reason to not exclude on case sensitive pattern

--- contrib/downloadboob/downloadboob.py
+++ contrib/downloadboob/downloadboob.py
@@ -96,7 +96,7 @@ class Downloadboob:
 
             if not self.is_downloaded(video):
                 self.backend.fill_video(video, ('url','title', 'url', 'duration'))
-                if not(self.is_excluded(video.title.lower(), title_exclude)) and self.id_regexp_matched(video.id, id_regexp):
+                if not(self.is_excluded(video.title, title_exclude)) and self.id_regexp_matched(video.id, id_regexp):
                     print "  %s\n    Id:%s\n    Duration:%s" % (video.title, video.id, video.duration)
                     videos.append(video)
             else:
@@ -113,7 +113,7 @@ class Downloadboob:
 
     def is_excluded(self, title, title_exclude):
         for exclude in title_exclude:
-            if title.find(exclude.lower()) > -1:
+            if title.find(exclude) > -1:
                 return True
         return False
 
