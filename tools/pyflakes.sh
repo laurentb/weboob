0aa4fd3ede5ee9a52d44bee5ed5b654ddde44c6b
Laurent Bachelier <laurent@bachelier.name>
2013-04-05 18:57:22 +0200

    Use only tracked Python files

--- tools/pyflakes.sh
+++ tools/pyflakes.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -u
 cd $(dirname $0)
 cd ..
-grep -r --include '*.py' 'class [^( ]\+:$' . && exit 3
-grep -r --include '*.py' $'\t\|\s$' . && exit 4
+PYFILES=$(git ls-files|grep '^scripts\|\.py$'|grep -v boilerplate_data|tr '\n' ' ')
+grep 'class [^( ]\+:$' ${PYFILES} && exit 3
+grep $'\t\|\s$' $PYFILES && exit 4
 
 FLAKE8=""
 if which flake8 >/dev/null 2>&1; then
@@ -14,9 +15,9 @@ fi
 
 if [ -n "${FLAKE8}" ]; then
     set -e
-    ${FLAKE8} --ignore=E,W --exclude='*_ui.py' *.py weboob modules contrib docs scripts/* tools/*.py
+    ${FLAKE8} --ignore=E,W --exclude='*_ui.py' *.py $PYFILES
 else
     # grep will return 0 only if it founds something, but our script
     # wants to return 0 when it founds nothing!
-    pyflakes *.py weboob modules contrib docs scripts/* tools/*.py | grep -v redefinition && exit 1 || exit 0
+    pyflakes $PYFILES | grep -v redefinition && exit 1 || exit 0
 fi
