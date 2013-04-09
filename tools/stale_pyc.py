11ed5938800be86e14783828fe0d76ff8ee6c040
Laurent Bachelier <laurent@bachelier.name>
2013-04-09 23:04:09 +0200

    Faster walk by excluding some directories

--- tools/stale_pyc.py
+++ tools/stale_pyc.py
@@ -4,7 +4,14 @@ import sys
 
 root = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
 verbose = '-v' in sys.argv
+excludes = ('.git', '.svn', '__pycache__')
+
 for dirpath, dirnames, filenames in os.walk(root):
+    for exclude in excludes:
+        try:
+            dirnames.remove(exclude)
+        except ValueError:
+            pass
     for filename in filenames:
         if filename.endswith('.pyc') or filename.endswith('pyo'):
             if not os.path.exists(os.path.join(dirpath, filename[:-1])):
