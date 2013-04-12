b05b789cf4546508b5f93665f73eacfee2b32561
Julien Veyssier <julien.veyssier@aiur.fr>
2013-04-12 14:10:38 +0200

    new subtitle backend: podnapisi

---
+++ modules/podnapisi/backend.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2013 Julien Veyssier
+#
+# This file is part of weboob.
+#
+# weboob is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# weboob is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with weboob. If not, see <http://www.gnu.org/licenses/>.
+
+from weboob.capabilities.subtitle import ICapSubtitle, LanguageNotSupported, Subtitle
+from weboob.applications.suboob.suboob import LANGUAGE_CONV
+from weboob.tools.backend import BaseBackend
+
+from .browser import PodnapisiBrowser
+
+from urllib import quote_plus
+
+__all__ = ['PodnapisiBackend']
+
+
+class PodnapisiBackend(BaseBackend, ICapSubtitle):
+    NAME = 'podnapisi'
+    MAINTAINER = u'Julien Veyssier'
+    EMAIL = 'julien.veyssier@aiur.fr'
+    VERSION = '0.g'
+    DESCRIPTION = 'Podnapisi movies and tv series subtitle website'
+    LICENSE = 'AGPLv3+'
+    BROWSER = PodnapisiBrowser
+
+    def create_default_browser(self):
+        return self.create_browser()
+
+    def get_subtitle(self, id):
+        return self.browser.get_subtitle(id)
+
+    def get_subtitle_file(self, id):
+        subtitle = self.browser.get_subtitle(id)
+        if not subtitle:
+            return None
+
+        return self.browser.openurl(subtitle.url.encode('utf-8')).read()
+
+    def iter_subtitles(self, language, pattern):
+        if language not in LANGUAGE_CONV.keys():
+            raise LanguageNotSupported()
+        return self.browser.iter_subtitles(language, quote_plus(pattern.encode('utf-8')))
+
+    def fill_subtitle(self, subtitle, fields):
+        if 'description' in fields or 'url' in fields:
+            sub = self.get_subtitle(subtitle.id)
+            subtitle.description = sub.description
+            subtitle.url = sub.url
+
+        return subtitle
+
+    OBJECTS = {
+        Subtitle: fill_subtitle,
+    }
---
+++ modules/podnapisi/browser.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2013 Julien Veyssier
+#
+# This file is part of weboob.
+#
+# weboob is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# weboob is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with weboob. If not, see <http://www.gnu.org/licenses/>.
+
+
+from weboob.tools.browser import BaseBrowser, BrowserHTTPNotFound
+from weboob.applications.suboob.suboob import LANGUAGE_CONV
+
+from .pages import SearchPage, SubtitlePage, LANGUAGE_NUMBERS
+
+
+__all__ = ['PodnapisiBrowser']
+
+
+class PodnapisiBrowser(BaseBrowser):
+    DOMAIN = 'www.podnapisi.net'
+    PROTOCOL = 'http'
+    ENCODING = 'utf-8'
+    USER_AGENT = BaseBrowser.USER_AGENTS['wget']
+    PAGES = {
+        'http://www.podnapisi.net/fr/ppodnapisi/search\?sJ=[0-9]*&sK=.*&sS=downloads&sO=desc': SearchPage,
+        'http://www.podnapisi.net/fr/ppodnapisi/podnapis/i/[0-9]*': SubtitlePage
+    }
+
+    def iter_subtitles(self, language, pattern):
+        nlang = LANGUAGE_NUMBERS[language]
+        self.location('http://www.podnapisi.net/fr/ppodnapisi/search?sJ=%s&sK=%s&sS=downloads&sO=desc' % (nlang, pattern.encode('utf-8')))
+        assert self.is_on_page(SearchPage)
+        return self.page.iter_subtitles(unicode(language))
+
+    def get_subtitle(self, id):
+        try:
+            self.location('http://www.podnapisi.net/fr/ppodnapisi/podnapis/i/%s' % id)
+        except BrowserHTTPNotFound:
+            return
+        if self.is_on_page(SubtitlePage):
+            return self.page.get_subtitle(id)
