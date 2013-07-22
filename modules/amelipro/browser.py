1fd0cb12914f8c1509e64d8d83b94fd6edeea95e
Christophe Lampin <weboob@lampin.net>
2013-07-22 16:11:29 +0200

    Add module [amelipro] for the heathcare professional ameli website
    Signed-off-by: Christophe Lampin <weboob@lampin.net>

---
+++ modules/amelipro/browser.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2013      Christophe Lampin
+#
+# This file is part of weboob.
+#
+# weboob is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# weboob is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with weboob. If not, see <http://www.gnu.org/licenses/>.
+
+import urllib
+import mechanize
+from weboob.tools.browser import BaseBrowser
+from weboob.capabilities.bill import Detail
+from decimal import *
+from .pages import LoginPage, HomePage, AccountPage, HistoryPage, BillsPage
+
+__all__ = ['AmeliProBrowser']
+
+class AmeliProBrowser(BaseBrowser):
+    PROTOCOL = 'https'
+    DOMAIN = 'espacepro.ameli.fr'
+    ENCODING = None
+
+    PAGES = {'.*_pageLabel=vp_login_page.*':  LoginPage,
+             '.*_pageLabel=vp_accueil.*': HomePage,
+             '.*_pageLabel=vp_coordonnees_infos_perso_page.*': AccountPage,
+             '.*_pageLabel=vp_recherche_par_date_paiements_page.*': HistoryPage,
+             '.*_pageLabel=vp_releves_mensuels_page.*': BillsPage,
+             }
+
+    loginp = '/PortailPS/appmanager/portailps/professionnelsante?_nfpb=true&_pageLabel=vp_login_page'
+    homep = '/PortailPS/appmanager/portailps/professionnelsante?_nfpb=true&_pageLabel=vp_accueil_book'
+    accountp = '/PortailPS/appmanager/portailps/professionnelsante?_nfpb=true&_pageLabel=vp_coordonnees_infos_perso_page'
+    billsp = '/PortailPS/appmanager/portailps/professionnelsante?_nfpb=true&_pageLabel=vp_releves_mensuels_page'
+    searchp = '/PortailPS/appmanager/portailps/professionnelsante?_nfpb=true&_pageLabel=vp_recherche_par_date_paiements_page'
+    historyp = '/PortailPS/appmanager/portailps/professionnelsante?_nfpb=true&_windowLabel=vp_recherche_paiement_tiers_payant_portlet_1&vp_recherche_paiement_tiers_payant_portlet_1_actionOverride=%2Fportlets%2Fpaiements%2Frecherche&_pageLabel=vp_recherche_par_date_paiements_page'
+
+    def home(self):
+        self.location(self.homep)
+
+    def is_logged(self):
+        if self.is_on_page(LoginPage):
+            return False
+        return True
+
+    def login(self):
+        assert isinstance(self.username, basestring)
+        assert isinstance(self.password, basestring)
+        if not self.is_on_page(LoginPage):
+            self.location(self.loginp)
+        self.page.login(self.username, self.password)
+        if self.is_on_page(LoginPage):
+            raise BrowserIncorrectPassword()
+
+    def get_subscription_list(self):
+        if not self.is_on_page(AccountPage):
+            self.location(self.accountp)
+        return self.page.get_subscription_list()
+
+    def get_subscription(self, id):
+        assert isinstance(id, basestring)
+        return self.get_subscription_list()
+
+    def iter_history(self, subscription):
+        if not self.is_on_page(HistoryPage):
+            self.location(self.searchp)
+
+        date_deb = self.page.document.xpath('//input[@name="vp_recherche_paiement_tiers_payant_portlet_1dateDebutRecherche"]')[0].value
+        date_fin = self.page.document.xpath('//input[@name="vp_recherche_paiement_tiers_payant_portlet_1dateFinRecherche"]')[0].value
+
+        data = {'vp_recherche_paiement_tiers_payant_portlet_1dateDebutRecherche': date_deb,
+                'vp_recherche_paiement_tiers_payant_portlet_1dateFinRecherche': date_fin,
+                'vp_recherche_paiement_tiers_payant_portlet_1codeOrganisme': 'null',
+                'vp_recherche_paiement_tiers_payant_portlet_1actionEvt': 'rechercheParDate',
+                'vp_recherche_paiement_tiers_payant_portlet_1codeRegime': '01',
+               }
+
+        self.location(self.historyp, urllib.urlencode(data))
+        return self.page.iter_history()
+
+    def get_details(self, sub):
+        det = Detail()
+        det.id = sub.id
+        det.label = sub.label
+        det.infos = ''
+        det.price = Decimal('0.0')
+        return det
+
+    def iter_bills(self):
+        if not self.is_on_page(BillsPage):
+            self.location(self.billsp)
+        return self.page.iter_bills()
+
+    def get_bill(self, id):
+        assert isinstance(id, basestring)
+        for b in self.get_bills():
+            if id == b.id:
+                return b
+        return None
---
+++ modules/amelipro/pages.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2013      Christophe Lampin
+#
+# This file is part of weboob.
+#
+# weboob is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# weboob is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with weboob. If not, see <http://www.gnu.org/licenses/>.
+
+
+from datetime import datetime
+from decimal import Decimal
+import re
+from weboob.tools.browser import BasePage
+from weboob.capabilities.bill import Subscription, Detail, Bill
+
+
+__all__ = ['LoginPage', 'HomePage', 'AccountPage', 'HistoryPage', 'BillsPage']
+
+# Ugly array to avoid the use of french locale
+FRENCH_MONTHS = [u'janvier', u'février', u'mars', u'avril', u'mai', u'juin', u'juillet', u'août', u'septembre', u'octobre', u'novembre', u'décembre']
+
+class LoginPage(BasePage):
+    def login(self, login, password):
+        self.browser.select_form('connexionCompteForm')
+        self.browser["vp_connexion_portlet_1numPS"] = login.encode('utf8')
+        self.browser["vp_connexion_portlet_1password"] = password.encode('utf8')
+        self.browser.submit()
+
+class HomePage(BasePage):
+
+    def on_loaded(self):
+        pass
+
+
+class AccountPage(BasePage):
+
+    def get_subscription_list(self):
+        ident = self.document.xpath('//div[@id="identification"]')[0]
+        prof = self.document.xpath('//div[@id="profession"]')[0]
+        name = ident.xpath('//p/b')[0].text.replace('&nbsp;', ' ').strip()
+        number = ident.xpath('//p')[1].text.replace('Cabinet', '').strip()
+        label = prof.xpath('//div[@class="zoneTexte"]')[0].text.strip()
+        sub = Subscription(number)
+        sub._id = number
+        sub.label = unicode(name) + ' ' + unicode(label)
+        sub.subscriber = unicode(name)
+        return sub
+
+class HistoryPage(BasePage):
+
+    def iter_history(self):
+        table = self.document.xpath('//table[contains(concat(" ", @class, " "), " cTableauTriable ")]')[0].xpath('.//tr')
+        for tr in table:
+            list_a = tr.xpath('.//a')
+            if len(list_a) == 0:
+                continue
+            date = tr.xpath('.//td')[0].text.strip()
+            lot = list_a[0].text
+            factures = tr.xpath('.//div[@class="cAlignGauche"]/a')
+            factures_lbl = ''
+            for a in factures:
+                factures_lbl = factures_lbl + a.text + ' '
+            montant = tr.xpath('.//div[@class="cAlignDroite"]')[0].text.strip()
+            det = Detail()
+            det.id = lot
+            det.label = lot
+            det.infos = factures_lbl
+            det.datetime = datetime.strptime(date, "%d/%m/%Y").date()
+            det.price = Decimal(montant.replace(',','.'))
+            yield det
+
+class BillsPage(BasePage):
+
+    def iter_bills(self):
+        table = self.document.xpath('//table[@id="releveCompteMensuel"]')[0].xpath('.//tr')
+        for tr in table:
+            list_tds = tr.xpath('.//td')
+            if len(list_tds) == 0:
+                continue
+
+            date_str = tr.xpath('.//td[@class="cAlignGauche"]')[0].text
+            month_str = date_str.split()[0]
+            date = datetime.strptime(re.sub(month_str,str(FRENCH_MONTHS.index(month_str) + 1),date_str),"%m %Y").date()
+            amount = tr.xpath('.//td[@class="cAlignDroite"]')[0].text
+            for format in ('CSV', 'PDF'):
+                bil = Bill()
+                bil.id = date.strftime("%Y%m") + format
+                bil.date = date
+                bil.label = u''+amount.strip()
+                bil.format = u''+format
+                filedate = date.strftime("%m%Y")
+                bil._url = '/PortailPS/fichier.do'
+                bil._args = {'FICHIER.type': format.lower() + '.releveCompteMensuel',
+                             'dateReleve': filedate,
+                             'FICHIER.titre': '',
+                }
+                yield bil
+
+    def get_bill(self,bill):
+       self.location(bill._url, urllib.urlencode(bill._args))
