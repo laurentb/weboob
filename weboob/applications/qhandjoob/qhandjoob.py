c470138540f069e9496f87a4cfe7ebfb817439e3
Florent <weboob@flo.fourcot.fr>
2013-08-03 00:07:02 +0200

    Fix encoding of copyright

--- weboob/applications/qhandjoob/qhandjoob.py
+++ weboob/applications/qhandjoob/qhandjoob.py
@@ -28,7 +28,7 @@ from .main_window import MainWindow
 class QHandJoob(QtApplication):
     APPNAME = 'qhandjoob'
     VERSION = '0.g'
-    COPYRIGHT = 'Copyright(C) 2010-2012 Sébastien Monel'
+    COPYRIGHT = u'Copyright(C) 2013 Sébastien Monel'
     DESCRIPTION = "Qt application to search for job."
     SHORT_DESCRIPTION = "search for job"
     CAPS = ICapJob
