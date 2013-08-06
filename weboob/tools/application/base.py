2406b26b027c1dd772173c8b7030529f08386e2f
Laurent Bachelier <laurent@bachelier.name>
2013-08-06 12:21:56 +0200

    Hide FormFieldConversionWarning too

--- weboob/tools/application/base.py
+++ weboob/tools/application/base.py
@@ -27,6 +27,7 @@ import tempfile
 import warnings
 
 from weboob.capabilities.base import ConversionWarning
+from weboob.tools.browser.browser import FormFieldConversionWarning
 from weboob.core import Weboob, CallErrors
 from weboob.core.backendscfg import BackendsConfig
 from weboob.tools.config.iconfig import ConfigError
@@ -338,6 +339,7 @@ class BaseApplication(object):
         # this only matters to developers
         if not self.options.debug and not self.options.save_responses:
             warnings.simplefilter('ignore', category=ConversionWarning)
+            warnings.simplefilter('ignore', category=FormFieldConversionWarning)
 
         handlers = []
 
