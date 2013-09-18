93129f5aed8b6794aedb12d40d106c0f05ddec7b
Florent <weboob@flo.fourcot.fr>
2013-09-18 13:38:18 +0200

    Add tests for poivy module

---
+++ modules/poivy/test.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2013  Fourcot Florent
+#
+# This file is part of weboob.
+#
+# weboob is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# weboob is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with weboob. If not, see <http://www.gnu.org/licenses/>.
+
+
+from weboob.tools.test import BackendTest
+
+
+__all__ = ['PoivyTest']
+
+
+class PoivyTest(BackendTest):
+    BACKEND = 'poivy'
+
+    def test_list(self):
+        """
+        Test listing of subscriptions .
+        No support of multi-account on the website, we could assume to
+        have only one subscription.
+        Check the balance if the subscription is ok.
+        """
+        subscriptions = list(self.backend.iter_subscription())
+        self.assertTrue(len(subscriptions) == 1, msg="Account listing failed")
+        self.assertTrue(self.backend.get_balance(subscriptions[0]) > 0,
+                        msg="Get balance failed")
+
+    def test_history(self):
+        for subscription in self.backend.iter_subscription():
+            self.assertTrue(len(list(self.backend.iter_bills_history(subscription))) > 0)
