965da2562e5a9db4b7f7c3740dd3c533c816e11f
Roger Philibert <roger.philibert@gmail.com>
2013-11-15 13:02:16 +0100

    add module jacquieetmichel

---
+++ modules/jacquieetmichel/browser.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2013 Roger Philibert
+#
+# This file is part of weboob.
+#
+# weboob is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# weboob is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with weboob. If not, see <http://www.gnu.org/licenses/>.
+
+import urllib
+
+from weboob.tools.browser import BaseBrowser
+from weboob.tools.browser.decorators import id2url
+
+from .video import JacquieEtMichelVideo
+from .pages import VideoPage, ResultsPage
+
+
+__all__ = ['JacquieEtMichelBrowser']
+
+
+class JacquieEtMichelBrowser(BaseBrowser):
+    DOMAIN = u'jacquieetmicheltv.net'
+    ENCODING = None
+    PAGES = {r'https?://.*jacquieetmicheltv.net/': ResultsPage,
+             r'https?://.*jacquieetmicheltv.net/videolist/.*': ResultsPage,
+             r'https?://.*jacquieetmicheltv.net/showvideo/(?P<id>\d+)/.*': VideoPage,
+            }
+
+    @id2url(JacquieEtMichelVideo.id2url)
+    def get_video(self, url, video=None):
+        self.location(url)
+        assert self.is_on_page(VideoPage), 'Should be on video page.'
+        return self.page.get_video(video)
+
+    def search_videos(self, pattern):
+        self.location('/videolist/searchmodevideo/query%s/' % (urllib.quote(pattern.encode('utf-8'))))
+        assert self.is_on_page(ResultsPage)
+        return self.page.iter_videos()
+
+    def latest_videos(self):
+        self.home()
+        assert self.is_on_page(ResultsPage)
+        return self.page.iter_videos()
---
+++ modules/jacquieetmichel/pages.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2013 Roger Philibert
+#
+# This file is part of weboob.
+#
+# weboob is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# weboob is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with weboob. If not, see <http://www.gnu.org/licenses/>.
+
+
+import re
+
+from weboob.capabilities.base import NotAvailable
+from weboob.tools.capabilities.thumbnail import Thumbnail
+from weboob.tools.browser import BasePage, BrokenPageError
+from weboob.tools.misc import to_unicode
+
+from .video import JacquieEtMichelVideo
+
+
+__all__ = ['ResultsPage', 'VideoPage']
+
+
+class ResultsPage(BasePage):
+    def iter_videos(self):
+        for span in self.document.xpath('//ul[@id="list"]/li'):
+            a = self.parser.select(span, 'a', 1)
+            url = a.attrib['href']
+            _id = re.sub(r'/showvideo/(\d+)/.*', r'\1', url)
+
+            video = JacquieEtMichelVideo(_id)
+
+            video.thumbnail = Thumbnail(unicode(span.find('.//img').attrib['src']))
+
+            title_el = self.parser.select(span, 'h2', 1)
+            video.title = to_unicode(title_el.text.strip())
+            video.description = self.parser.tocleanstring(span.xpath('.//div[@class="desc"]')[0])
+            video.set_empty_fields(NotAvailable, ('url,'))
+
+            yield video
+
+
+class VideoPage(BasePage):
+    def get_video(self, video=None):
+        _id = to_unicode(self.group_dict['id'])
+        if video is None:
+            video = JacquieEtMichelVideo(_id)
+        title_el = self.parser.select(self.document.getroot(), 'h1', 1)
+        video.title = to_unicode(title_el.text.strip())
+        video.description = self.document.xpath('//meta[@name="description"]')[0].attrib['content']
+
+        for script in self.document.xpath('.//script'):
+            if script.text is None:
+                continue
+            m = re.search('"(http://[^"]+.mp4)"', script.text, re.MULTILINE)
+            if m:
+                video.url = to_unicode(m.group(1))
+                break
+
+        if not video.url:
+            raise BrokenPageError('Unable to find URL')
+
+        video.set_empty_fields(NotAvailable)
+
+        return video
