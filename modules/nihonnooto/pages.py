da0ecfd6a7d3560ccf23088112bd4c770549a48a
Florent <weboob@flo.fourcot.fr>
2013-12-16 12:19:04 +0100

    pep8 nihonnooto

--- modules/nihonnooto/pages.py
+++ modules/nihonnooto/pages.py
@@ -23,6 +23,7 @@ from weboob.capabilities.audiostream import BaseAudioStream, AudioStreamInfo
 
 __all__ = ['LivePage', 'ProgramPage']
 
+
 class LivePage(BasePage):
     def iter_radios_list(self):
         radio = Radio('nihon')
@@ -34,20 +35,19 @@ class LivePage(BasePage):
 
         for el in self.document.xpath('//source'):
             index += 1
-            mime_type  = unicode(el.attrib['type'])
+            mime_type = unicode(el.attrib['type'])
             stream_url = unicode(el.attrib['src'])
             stream = BaseAudioStream(index)
-            stream.bitrate=128
-            if ( mime_type == u'audio/mpeg' ):
-                stream.format=u'mp3'
-            elif ( mime_type == u'audio/ogg' ):
-                stream.format=u'vorbis'
+            stream.bitrate = 128
+            if (mime_type == u'audio/mpeg'):
+                stream.format = u'mp3'
+            elif (mime_type == u'audio/ogg'):
+                stream.format = u'vorbis'
             stream.title = radio.title + ' ' + mime_type
             stream.url = stream_url
             radio.streams.append(stream)
 
         yield radio
-
 
 
 class ProgramPage(BasePage):
@@ -56,8 +56,8 @@ class ProgramPage(BasePage):
         two_or_more = unicode(self.document.xpath('//p')[0].text).split('/////')[0].split(' - ')
         # Consider that if String(' - ') appears it'll be in title rather in the artist name
         if len(two_or_more) > 2:
-          current.who = two_or_more.pop(0)
-          current.what = ' - '.join(two_or_more)
+            current.who = two_or_more.pop(0)
+            current.what = ' - '.join(two_or_more)
         else:
-          current.who, current.what = two_or_more
+            current.who, current.what = two_or_more
         return current
