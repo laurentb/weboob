4f4d08ec33c102d799a4612412f2adb0d3d5fcbd
Bezleputh <carton_ben@yahoo.fr>
2014-03-15 10:23:26 +0100

    [browser2] Allow to import Profile

--- weboob/tools/browser2/__init__.py
+++ weboob/tools/browser2/__init__.py
@@ -17,9 +17,9 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with weboob. If not, see <http://www.gnu.org/licenses/>.
 
-from .browser import BaseBrowser, DomainBrowser, Wget, Firefox, UrlNotAllowed
+from .browser import BaseBrowser, DomainBrowser, Wget, Firefox, UrlNotAllowed, Profile
 from .page import PagesBrowser, BasePage, URL, HTMLPage, LoginBrowser, need_login
 
 
-__all__ = ['BaseBrowser', 'DomainBrowser', 'Wget', 'Firefox', 'UrlNotAllowed',
+__all__ = ['BaseBrowser', 'DomainBrowser', 'Wget', 'Firefox', 'UrlNotAllowed', 'Profile',
            'PagesBrowser', 'BasePage', 'URL', 'HTMLPage', 'LoginBrowser', 'need_login']
