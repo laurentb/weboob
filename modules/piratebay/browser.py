d5ea0d08105cf2432023ace6b8a15047e1a06bc7
Laurent Bachelier <laurent@bachelier.name>
2014-03-18 15:34:08 +0100

    piratebay: yet another domain change

--- modules/piratebay/browser.py
+++ modules/piratebay/browser.py
@@ -34,7 +34,7 @@ class PiratebayBrowser(BaseBrowser):
     ENCODING = 'utf-8'
 
     def __init__(self, url, *args, **kwargs):
-        url = url or 'https://thepiratebay.pe/'
+        url = url or 'https://thepiratebay.se/'
         url_parsed = urlsplit(url)
         self.PROTOCOL = url_parsed.scheme
         self.DOMAIN = url_parsed.netloc
