0ff2abb2fd636ab5a5e5acba5d32728e3f10d5f7
Romain Bignon <romain@budget-insight.com>
2014-05-19 09:25:21 +0200

    don't crash when quantities are >999

--- modules/ing/pages/titre.py
+++ modules/ing/pages/titre.py
@@ -48,7 +48,7 @@ class TitrePage(RawPage):
             invest.code = unicode(code)
             invest.label = unicode(columns[0].split('{')[-1])
             # XXX sometimes there are decimal (!) quantities
-            invest.quantity = int(columns[1].split(',')[0])
+            invest.quantity = int(columns[1].split(',')[0].replace(' ', ''))
             invest.unitprice = Decimal(FrenchTransaction.clean_amount(columns[2]))
             invest.unitvalue = Decimal(FrenchTransaction.clean_amount(columns[3]))
             invest.valuation = Decimal(FrenchTransaction.clean_amount(columns[4]))
