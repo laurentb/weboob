20ffb6ca08b4060fad43ca5f192fa9f2ed75f423
Florent <weboob@flo.fourcot.fr>
2014-07-09 12:01:35 +0200

    Revert part of "import module used only in debug mode"
    Since the date are now not always imported, it does not make a lot of
    sense to not import all date utils

--- weboob/tools/date.py
+++ weboob/tools/date.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with weboob. If not, see <http://www.gnu.org/licenses/>.
 
-
+import dateutil.parser
 from datetime import date as real_date, datetime as real_datetime, timedelta
 import time
 import re
@@ -263,7 +263,6 @@ def parse_french_date(date):
     for fr, en in DATE_TRANSLATE_FR:
         date = fr.sub(en, date)
 
-    import dateutil.parser
     return dateutil.parser.parse(date)
 
 
