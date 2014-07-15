0d388297274e3aef076264e8714ec96eb21dba2a
Bezleputh <carton_ben@yahoo.fr>
2014-07-15 08:47:37 +0200

    [biplan] handle summer holiday in tests

--- modules/biplan/test.py
+++ modules/biplan/test.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with weboob. If not, see <http://www.gnu.org/licenses/>.
 
-
+from nose.plugins.skip import SkipTest
 from weboob.tools.test import BackendTest
 from datetime import datetime
 
@@ -26,6 +26,8 @@ class BiplanTest(BackendTest):
     BACKEND = 'biplan'
 
     def test_biplan_list(self):
+        if datetime.now() > datetime(datetime.now().year, 7, 14) and datetime.now() < datetime(datetime.now().year, 9, 15):
+            raise SkipTest("Fermeture estivale")
         l = list(self.backend.list_events(datetime.now()))
         assert len(l)
         event = self.backend.get_event(l[0].id)
