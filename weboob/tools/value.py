8a25c759991617fbc336b82fcd1c238090e1e7cf
Florent <weboob@flo.fourcot.fr>
2014-09-02 12:46:04 +0200

    Revert "be case insensitive on value choices (closes #1319)"
    This reverts commit ebb87844f3224f42f02eb45d354bccc83be7bb32.

--- weboob/tools/value.py
+++ weboob/tools/value.py
@@ -89,7 +89,7 @@ class Value(object):
             raise ValueError('Value can\'t be empty')
         if self.regexp is not None and not re.match(self.regexp, unicode(v)):
             raise ValueError('Value "%s" does not match regexp "%s"' % (v, self.regexp))
-        if self.choices is not None and not v.lower() in map(str.lower, self.choices.iterkeys()):
+        if self.choices is not None and not v in self.choices.iterkeys():
             raise ValueError('Value "%s" is not in list: %s' % (
                 v, ', '.join(unicode(s) for s in self.choices.iterkeys())))
 
