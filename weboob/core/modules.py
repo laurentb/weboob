4d63955cb582a5bdf0b13b90b22be6ac3a59a8de
Laurent Bachelier <laurent@bachelier.name>
2014-09-10 14:05:08 +0200

    Put back wrongly removed __all__

--- weboob/core/modules.py
+++ weboob/core/modules.py
@@ -25,6 +25,9 @@ from weboob.tools.backend import BaseBackend
 from weboob.tools.log import getLogger
 
 
+__all__ = ['Module', 'ModulesLoader', 'RepositoryModulesLoader', 'ModuleLoadError']
+
+
 class ModuleLoadError(Exception):
     def __init__(self, module_name, msg):
         Exception.__init__(self, msg)
