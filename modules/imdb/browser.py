5506fdb46e67a676cf331389f8f479c0012448d9
Julien Veyssier <julien.veyssier@aiur.fr>
2014-11-24 18:43:18 +0100

    [imdb] fix 'N/A' value in movie release date

--- modules/imdb/browser.py
+++ modules/imdb/browser.py
@@ -125,24 +125,27 @@ class ImdbBrowser(Browser):
             if m:
                 duration = int(m.group(1))
         if 'Released' in jres:
-            months = {
-                    'Jan':'01',
-                    'Feb':'02',
-                    'Mar':'03',
-                    'Apr':'04',
-                    'May':'05',
-                    'Jun':'06',
-                    'Jul':'07',
-                    'Aug':'08',
-                    'Sep':'09',
-                    'Oct':'10',
-                    'Nov':'11',
-                    'Dec':'12',
-                     }
             released_string = str(jres['Released'])
-            for st in months:
-                released_string = released_string.replace(st,months[st])
-            release_date = datetime.strptime(released_string, '%d %m %Y')
+            if released_string == 'N/A':
+                release_date = NotAvailable
+            else:
+                months = {
+                        'Jan':'01',
+                        'Feb':'02',
+                        'Mar':'03',
+                        'Apr':'04',
+                        'May':'05',
+                        'Jun':'06',
+                        'Jul':'07',
+                        'Aug':'08',
+                        'Sep':'09',
+                        'Oct':'10',
+                        'Nov':'11',
+                        'Dec':'12',
+                         }
+                for st in months:
+                    released_string = released_string.replace(st,months[st])
+                release_date = datetime.strptime(released_string, '%d %m %Y')
         if 'Country' in jres:
             country = u''
             for c in jres['Country'].split(', '):
