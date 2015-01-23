387124522f538564b501ebbc4af92625c5abcb43
Romain Bignon <romain@symlink.me>
2015-01-23 16:34:27 +0100

    make all attributes of optim start with an underscore

--- modules/okc/optim/profiles_walker.py
+++ modules/okc/optim/profiles_walker.py
@@ -29,77 +29,71 @@ class ProfilesWalker(Optimization):
     CONFIG = ValuesDict(Value('first_message', label='First message to send to matched profiles', default=''))
 
     def __init__(self, sched, storage, browser):
-        self.sched = sched
-        self.storage = storage
-        self.browser = browser
-        self.logger = getLogger('walker', browser.logger)
+        self._sched = sched
+        self._storage = storage
+        self._browser = browser
+        self._logger = getLogger('walker', browser.logger)
 
-        self.config = storage.get('profile_walker', 'config', default=None)
-        if self.config == {}:
-            self.config = None
+        self._config = storage.get('profile_walker', 'config', default=None)
+        if self._config == {}:
+            self._config = None
 
-        self.view_cron = None
-        self.visited_profiles = set(storage.get('profiles_walker', 'viewed'))
-        self.logger.info(u'Loaded %d already visited profiles from storage.' % len(self.visited_profiles))
-        self.profiles_queue = set()
+        self._view_cron = None
+        self._visited_profiles = set(storage.get('profiles_walker', 'viewed'))
+        self._logger.info(u'Loaded %d already visited profiles from storage.' % len(self._visited_profiles))
+        self._profiles_queue = set()
 
     def save(self):
-        if self.config is None:
+        if self._config is None:
             return False
 
-        self.storage.set('profiles_walker', 'viewed', list(self.visited_profiles))
-        self.storage.save()
+        self._storage.set('profiles_walker', 'viewed', list(self._visited_profiles))
+        self._storage.save()
 
     def start(self):
-        self.view_cron = self.sched.schedule(randint(5, 10), self.view_profile)
+        self._view_cron = self._sched.schedule(randint(5, 10), self.view_profile)
         return True
 
     def stop(self):
-        self.sched.cancel(self.view_cron)
-        self.view_cron = None
+        self._sched.cancel(self._view_cron)
+        self._view_cron = None
         return True
 
     def is_running(self):
-        return self.view_cron is not None
+        return self._view_cron is not None
 
     def set_config(self, params):
-        self.config = params
-        self.storage.set('profile_walker', 'config', self.config)
-        self.storage.save()
+        self._config = params
+        self._storage.set('profile_walker', 'config', self._config)
+        self._storage.save()
 
     def get_config(self):
-        return self.config
+        return self._config
 
     def view_profile(self):
         try:
-            id = self.browser.find_match_profile()
-            if id in self.visited_profiles:
+            id = self._browser.find_match_profile()
+            if id in self._visited_profiles:
                 return
 
             try:
-                with self.browser:
-                    # profile = self.browser.get_profile(id)
-                    self.browser.do_rate(id)
-                    self.browser.visit_profile(id)
-                    if self.config['first_message'] != '':
-                        self.browser.post_mail(id, unicode(self.config['first_message']))
-                self.logger.info(u'Visited profile %s ' % (id))
-
-                # Get score from the aum_score module
-                #d = self.nucentral_core.callService(context.Context.fromComponent(self), 'aum_score', 'score', profile)
-                # d.addCallback(self.score_cb, profile.getID())
-                # deferredlist.append(d)
+                with self._browser:
+                    self._browser.do_rate(id)
+                    self._browser.visit_profile(id)
+                    if self._config['first_message'] != '':
+                        self._browser.post_mail(id, unicode(self._config['first_message']))
+                self._logger.info(u'Visited profile %s ' % (id))
 
                 # do not forget that we visited this profile, to avoid re-visiting it.
-                self.visited_profiles.add(id)
+                self._visited_profiles.add(id)
                 self.save()
 
             except BrowserUnavailable:
                 # We consider this profil hasn't been [correctly] analysed
-                self.profiles_queue.add(id)
+                self._profiles_queue.add(id)
                 return
             except Exception as e:
-                self.logger.exception(e)
+                self._logger.exception(e)
         finally:
-            if self.view_cron is not None:
-                self.view_cron = self.sched.schedule(randint(5, 10), self.view_profile)
+            if self._view_cron is not None:
+                self._view_cron = self._sched.schedule(randint(5, 10), self.view_profile)
