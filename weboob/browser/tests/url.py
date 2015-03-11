f13ab8efa4a316dff1c7c39c98d9874962beb259
Laurent Bachelier <laurent@bachelier.name>
2015-03-11 12:45:47 +0100

    browser: Use valid URLs in tests

--- weboob/browser/tests/url.py
+++ weboob/browser/tests/url.py
@@ -23,8 +23,8 @@ from weboob.browser.url import UrlNotResolvable
 
 
 class MyMockBrowserWithoutBrowser(object):
-    BASEURL = "http://weboob.org"
-    url = URL("http://test.org")
+    BASEURL = "http://weboob.org/"
+    url = URL("http://test.org/")
 
 
 # Mock that allows to represent a Page
@@ -34,22 +34,22 @@ class MyMockPage(Page):
 
 # Mock that allows to represent a Browser
 class MyMockBrowser(PagesBrowser):
-    BASEURL = "http://weboob.org"
+    BASEURL = "http://weboob.org/"
 
     # URL used by method match
-    urlNotRegex = URL("http://test.org", "http://test2.org")
-    urlRegex = URL("http://test.org", "http://weboob2.org")
+    urlNotRegex = URL("http://test.org/", "http://test2.org/")
+    urlRegex = URL("http://test.org/", "http://weboob2.org/")
     urlRegWithoutHttp = URL("news")
     urlNotRegWithoutHttp = URL("youtube")
 
     # URL used by method build
     urlValue = URL("http://test.com/(?P<id>\d+)")
-    urlParams = URL("http://test.com\?id=(?P<id>\d+)&name=(?P<name>.+)")
+    urlParams = URL("http://test.com/\?id=(?P<id>\d+)&name=(?P<name>.+)")
     urlSameParams = URL("http://test.com/(?P<id>\d+)", "http://test.com\?id=(?P<id>\d+)&name=(?P<name>.+)")
 
     # URL used by method is_here
     urlIsHere = URL('http://weboob.org/(?P<param>)', MyMockPage)
-    urlIsHereDifKlass = URL('http://free.fr', MyMockPage)
+    urlIsHereDifKlass = URL('http://free.fr/', MyMockPage)
 
 
 # Class that tests different methods from the class URL
@@ -64,14 +64,14 @@ class URLTest(TestCase):
     def test_match_base_none_browser_none(self):
         self.assertRaises(AssertionError,
                           self.myBrowserWithoutBrowser.url.match,
-                          "http://weboob.org")
+                          "http://weboob.org/")
 
     # Check that no assert is raised when browser is none and a base is indeed
     # instanciated when given as a parameter
     def test_match_base_not_none_browser_none(self):
         try:
             self.myBrowserWithoutBrowser.url.match("http://weboob.org/news",
-                                                   "http://weboob.org")
+                                                   "http://weboob.org/")
         except AssertionError:
             self.fail("Method match returns an AssertionError while" +
                       " base parameter is not none!")
@@ -109,7 +109,7 @@ class URLTest(TestCase):
     # identifiers and values of some parameters
     def test_build_urlParams_OK(self):
         res = self.myBrowser.urlParams.build(id=2, name="weboob")
-        self.assertEquals(res, "http://test.com?id=2&name=weboob")
+        self.assertEquals(res, "http://test.com/?id=2&name=weboob")
 
     # Checks that build returns the right url when it needs to add
     # identifiers and values of some parameters.
