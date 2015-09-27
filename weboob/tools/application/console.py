9baccd17e00fb37063b66de260f799f1df57b217
nojhan <nojhan@nojhan.net>
2015-09-27 20:34:59 +0200

    bugfix: create a message for 403 errors
    When handling a BrowserForbidden error, the msg variable was not initialized,
    thus leading to a reference before assignement error.
    
    Signed-off-by: nojhan <nojhan@nojhan.net>

--- weboob/tools/application/console.py
+++ weboob/tools/application/console.py
@@ -569,6 +569,7 @@ class ConsoleApplication(Application):
         elif isinstance(error, BrowserSSLError):
             print(u'FATAL(%s): ' % backend.name + self.BOLD + '/!\ SERVER CERTIFICATE IS INVALID /!\\' + self.NC, file=self.stderr)
         elif isinstance(error, BrowserForbidden):
+            msg = unicode(error)
             print(u'Error(%s): %s' % (backend.name, msg or 'Forbidden'), file=self.stderr)
         elif isinstance(error, BrowserUnavailable):
             msg = unicode(error)
