ea560d7c141c44691487bfd6ae846b7ec4023a52
Romain Bignon <romain@budget-insight.com>
2015-11-05 15:54:33 +0100

    disable lowsslcheck if there is a proxy, as ssl doesn't deal with it

--- weboob/deprecated/browser/browser.py
+++ weboob/deprecated/browser/browser.py
@@ -401,7 +401,7 @@ class StandardBrowser(mechanize.Browser):
             return
 
     def lowsslcheck(self, domain, hsh):
-        if self.INSECURE or (self.logger is not None and self.logger.settings['ssl_insecure']):
+        if self.INSECURE or (self.logger is not None and self.logger.settings['ssl_insecure']) or self.proxy is not None:
             return
         certhash = self._certhash(domain)
         if self.logger:
