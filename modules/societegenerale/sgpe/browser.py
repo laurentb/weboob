7803ac449d61589b642a7e99d0cdc572300216b6
Vincent Paredes <vparedes@budget-insight.com>
2015-11-19 15:03:53 +0100

    new ssl certificate

--- modules/societegenerale/sgpe/browser.py
+++ modules/societegenerale/sgpe/browser.py
@@ -159,4 +159,4 @@ class SGEnterpriseBrowser(SGPEBrowser):
     DOMAIN = 'entreprises.secure.societegenerale.fr'
     LOGIN_FORM = 'auth'
     MENUID = 'BANREL'
-    CERTHASH = '4c7e609ead0772f3702ae139f0c29bdf8050fa9c6d7a1edb6f21ff3bf04ba84c'
+    CERTHASH = '2231d5ddb97d2950d5e6fc4d986c23be4cd231c31ad530942343a8fdcc44bb99'
