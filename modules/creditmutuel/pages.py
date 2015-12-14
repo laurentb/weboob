be50d3d5a1757112c998cd4faa559639e5f41f78
Baptiste Delpey <bdelpey@budget-insight.fr>
2015-12-14 18:49:14 +0100

    fix wrong valuation on market accounts if several are present

--- modules/creditmutuel/pages.py
+++ modules/creditmutuel/pages.py
@@ -392,7 +392,7 @@ class PorPage(LoggedPage, HTMLPage):
 
     def fill(self, acc):
         self.send_form(acc)
-        ele = self.doc.xpath('.//table[@class="fiche bourse"]//td[contains(@id, "Valorisation")]')[0]
+        ele = self.browser.page.doc.xpath('.//table[@class="fiche bourse"]//td[contains(@id, "Valorisation")]')[0]
         balance = CleanDecimal(ele.xpath('.'), default=Decimal(0), replace_dots=True)(ele)
         currency = FrenchTransaction.Currency('.')(ele)
         return balance, currency
