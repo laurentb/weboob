12189e57624401668ce464dbd564892d6a013f30
Baptiste Delpey <bdelpey@budget-insight.fr>
2015-12-16 16:33:16 +0100

    fix parsing of rdate

--- modules/americanexpress/pages.py
+++ modules/americanexpress/pages.py
@@ -153,9 +153,9 @@ class TransactionsPage(Page):
             except BrokenPageError:
                 pass
             else:
-                rday, rmonth = (' '.join([txt.strip() for txt in detail.itertext()])).strip().split(' ')[3:5]
+                rday, rmonth = re.search(r' (\d{2} .{3,4})\.', (' '.join([txt.strip() for txt in detail.itertext()])).strip()).group(1).split(' ')
                 rday = int(rday)
-                rmonth = self.MONTHS.index(rmonth.rstrip('.')) + 1
+                rmonth = self.MONTHS.index(rmonth) + 1
                 rdate = guesser.guess_date(rday, rmonth)
                 detail.drop_tree()
 
