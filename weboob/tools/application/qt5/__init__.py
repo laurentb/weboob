a905a6c3128db671848c98f7872fc2b603cef480
Vincent A <dev@indigo.re>
2016-01-27 20:51:10 +0100

    introduce weboob.tools.application.qt5 package

---
+++ weboob/tools/application/qt5/__init__.py
@@ -0,0 +1,5 @@
+from .qt import QtApplication, QtMainWindow, QtDo, HTMLDelegate
+from .backendcfg import BackendCfg
+
+__all__ = ['QtApplication', 'QtMainWindow', 'QtDo', 'HTMLDelegate',
+           'BackendCfg']
