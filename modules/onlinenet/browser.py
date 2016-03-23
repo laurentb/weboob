b46bc44f5dec56f6b668efe8eff978d5f6959950
Edouard Lambert <elambert@budget-insight.com>
2016-03-23 17:50:45 +0100

    adding online.net module

---
+++ modules/onlinenet/browser.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2016      Edouard Lambert
+#
+# This file is part of weboob.
+#
+# weboob is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# weboob is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with weboob. If not, see <http://www.gnu.org/licenses/>.
+
+
+from weboob.browser import LoginBrowser, URL, need_login
+from weboob.exceptions import BrowserIncorrectPassword
+
+from .pages import LoginPage, ProfilPage, DocumentsPage
+
+
+class OnlinenetBrowser(LoginBrowser):
+    BASEURL = 'https://console.online.net/en/'
+
+    login = URL('login', LoginPage)
+    profil = URL('account/edit', ProfilPage)
+    documents = URL('bill/list', DocumentsPage)
+
+    def do_login(self):
+        self.login.go()
+
+        self.page.login(self.username, self.password)
+
+        if self.login.is_here():
+            raise BrowserIncorrectPassword
+
+    @need_login
+    def get_subscription_list(self):
+        return self.profil.stay_or_go().get_list()
+
+    @need_login
+    def iter_documents(self, subscription):
+        for b in self.documents.stay_or_go().get_bills():
+            yield b
+        for d in self.documents.stay_or_go().get_documents():
+            yield d
---
+++ modules/onlinenet/pages.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2016      Edouard Lambert
+#
+# This file is part of weboob.
+#
+# weboob is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# weboob is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with weboob. If not, see <http://www.gnu.org/licenses/>.
+
+
+import re
+
+from weboob.browser.pages import HTMLPage, LoggedPage
+from weboob.browser.filters.standard import CleanText, CleanDecimal, Env, Format, TableCell, Date
+from weboob.browser.elements import ListElement, ItemElement, TableElement, method
+from weboob.browser.filters.html import Attr
+from weboob.capabilities.bill import Bill, Document, Subscription
+from weboob.capabilities.base import NotAvailable
+
+
+class LoginPage(HTMLPage):
+    def login(self, login, password):
+        form = self.get_form('//form[@id="login"]')
+        form['_username'] = login
+        form['_password'] = password
+        form.submit()
+
+
+class ProfilPage(LoggedPage, HTMLPage):
+    @method
+    class get_list(ListElement):
+        class item(ItemElement):
+            klass = Subscription
+
+            obj_subscriber = Format('%s %s', CleanText('//label[@for="form_firstname"]/../following-sibling::div'), CleanText('//label[@for="form_firstname"]/../following-sibling::div'))
+            obj_id = Env('username')
+            obj_label = obj_id
+
+            def parse(self, el):
+                self.env['username'] = self.page.browser.username
+
+
+class DocumentsPage(LoggedPage, HTMLPage):
+    @method
+    class get_bills(TableElement):
+        item_xpath = '//h3[contains(text(), "bills")]/following-sibling::table//tr[position() > 1]'
+        head_xpath = '//h3[contains(text(), "bills")]/following-sibling::table//tr/th'
+
+        col_id = u'Id'
+        col_date = u'Date'
+        col_price = u'Total TTC'
+
+        class item(ItemElement):
+            klass = Bill
+
+            obj_id = Format('%s_%s', Env('username'), CleanDecimal(TableCell('id')))
+            obj__url = Attr('.//a[contains(text(), "PDF")]', 'href', default=NotAvailable)
+            obj_date = Date(CleanText(TableCell('date')))
+            obj_format = u"pdf"
+            obj_label = Format('Facture %s', CleanDecimal(TableCell('id')))
+            obj_type = u"bill"
+            obj_price = CleanDecimal(TableCell('price'))
+            obj_currency = u"€"
+
+            def parse(self, el):
+                self.env['username'] = self.page.browser.username
+
+
+    @method
+    class get_documents(ListElement):
+        item_xpath = '//a[contains(@href, ".pdf")]'
+
+        class item(ItemElement):
+            klass = Document
+
+            obj_id = Format('%s_%s', Env('username'), Env('docid'))
+            obj__url = Attr('.', 'href')
+            obj_format = u"pdf"
+            obj_label = CleanText('.')
+            obj_type = u"other"
+
+            def parse(self, el):
+                self.env['username'] = self.page.browser.username
+                self.env['docid'] = re.sub('[^a-zA-Z0-9-_*.]', '', CleanText('.')(self))
