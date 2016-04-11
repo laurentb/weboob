91a2efef19c7a3fea6659ca8e4ba23bbb40c80de
Baptiste Delpey <bdelpey@budget-insight.fr>
2016-04-11 10:47:13 +0200

    take custom-id if regular ids are not present

--- modules/boursorama/pages.py
+++ modules/boursorama/pages.py
@@ -168,7 +168,7 @@ class HistoryPage(LoggedPage, HTMLPage):
         class item(ItemElement):
             klass = Transaction
 
-            obj_id = Attr('.', 'data-id')
+            obj_id = Attr('.', 'data-id', default=NotAvailable) or Attr('.', 'data-custom-id')
             obj_raw = Transaction.Raw(CleanText('.//div[contains(@class, "label__name")]'))
             obj_date = Date(Attr('.//time', 'datetime'))
             obj_amount = CleanDecimal('.//div[contains(@class, "amount")]', replace_dots=True)
