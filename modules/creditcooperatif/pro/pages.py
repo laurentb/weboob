bd4e37f8604f83c04373685aafb993667f249799
Baptiste Delpey <bdelpey@budget-insight.fr>
2016-04-26 11:53:00 +0200

    limit password to 12 chars

--- modules/creditcooperatif/pro/pages.py
+++ modules/creditcooperatif/pro/pages.py
@@ -34,7 +34,7 @@ class LoginPage(Page):
 
         self.browser.select_form(name='loginCoForm', nr=form_nb)
         self.browser['codeUtil'] = login.encode(self.browser.ENCODING)
-        self.browser['motPasse'] = pin.encode(self.browser.ENCODING)
+        self.browser['motPasse'] = pin[:12].encode(self.browser.ENCODING)
 
         assert self.browser['identType'] == indentType
         self.browser.submit(nologin=True)
