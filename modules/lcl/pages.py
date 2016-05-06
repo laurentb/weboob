96f33f31089acbea661f117b055de42fb7bcaaf5
Baptiste Delpey <bdelpey@budget-insight.fr>
2016-05-06 17:50:11 +0200

    balance and valuation

--- modules/lcl/pages.py
+++ modules/lcl/pages.py
@@ -361,7 +361,7 @@ class BoursePage(LoggedPage, HTMLPage):
                 ac_code = ac_code.strip().split(' - ')
                 if a.id == '%s%s' % (ac_code[0], ac_code[1]):
                     a._market_link = Regexp(CleanText('.//td[2]/@onclick'), "'(.*?)'")(tr)
-                    a.balance += CleanDecimal('.//td[has-class("last")]', replace_dots=True)(tr)
+                    a.valuation = CleanDecimal('.//td[has-class("last")]', replace_dots=True)(tr)
             yield a
 
     @method
