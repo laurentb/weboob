f36f319dcab9b54d9043595c3372eb4d66a41d4d
Baptiste Delpey <bdelpey@budget-insight.fr>
2016-05-06 11:40:49 +0200

    fix transactions detail link

--- modules/paypal/browser.py
+++ modules/paypal/browser.py
@@ -205,7 +205,7 @@ class Paypal(LoginBrowser):
         raise NotImplementedError()
 
     def convert_amount(self, account, trans, link):
-        self.location('%s%s' % (self.BASEURL, link))
+        self.location('%s%s' % (self.BASEURL, link.replace(self.BASEURL, '')))
         if self.history_details.is_here():
             cc = self.page.get_converted_amount(account)
             return cc
