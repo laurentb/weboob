6e0aaf249082cc9b2d8a0c2dba1b45e4f5f34d6a
Edouard Lambert <elambert@budget-insight.com>
2016-11-04 18:15:38 +0100

    fixing fortuneo : No IBAN for checking account

--- modules/fortuneo/pages/accounts_list.py
+++ modules/fortuneo/pages/accounts_list.py
@@ -311,6 +311,10 @@ class AccountsList(Page):
                 if pattern in account._link_id:
                     account.type = type
 
+            if account.type in (Account.TYPE_CHECKING, Account.TYPE_SAVINGS):
+                # Pour y accéder, vous devez renseigner un code sécurité à usage unique qui vous sera envoyé par SMS
+                account.iban = NotAvailable
+
             if (account.label, account.id, account.balance) not in [(a.label, a.id, a.balance) for a in accounts]:
                 accounts.append(account)
 
