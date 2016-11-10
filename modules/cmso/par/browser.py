8b9fa35cb46106960dfc955d9fa0adb8950337f3
Jean Walrave <jean.walrave@gmail.com>
2016-11-10 11:55:47 +0100

    cmmc/cmso: more history is now recovered

--- modules/cmso/par/browser.py
+++ modules/cmso/par/browser.py
@@ -18,7 +18,7 @@
 # along with weboob. If not, see <http://www.gnu.org/licenses/>.
 
 
-import re, json
+import re, json, datetime
 
 from weboob.browser import LoginBrowser, URL, need_login
 from weboob.exceptions import BrowserIncorrectPassword
@@ -42,6 +42,7 @@ class CmsoParBrowser(LoginBrowser):
 
     def __init__(self, website, *args, **kwargs):
         super(CmsoParBrowser, self).__init__(*args, **kwargs)
+
         self.BASEURL = "https://mon.%s" % website
         self.name = website.split('.')[0]
         self.website = website
@@ -52,6 +53,7 @@ class CmsoParBrowser(LoginBrowser):
     def deinit(self):
         if self.page.logged:
             self.logout.go(method='DELETE')
+
         super(CmsoParBrowser, self).deinit()
 
     def do_login(self):
@@ -62,12 +64,14 @@ class CmsoParBrowser(LoginBrowser):
             'redirectUri': '%s/auth/checkuser' % self.BASEURL,
             'errorUri': '%s/auth/errorauthn' % self.BASEURL
         }
+
         self.location('/securityapi/tokens', data=data)
 
         if self.logout.is_here():
             raise BrowserIncorrectPassword
 
         m = re.search('access_token=([^&]+).*id_token=(.*)', self.url)
+
         self.session.headers.update({
             'Content-Type': 'application/json',
             'Authentication': "Bearer %s" % m.group(2),
@@ -80,13 +84,14 @@ class CmsoParBrowser(LoginBrowser):
     def iter_accounts(self):
         # First get all checking accounts...
         data = dict(self.infos.stay_or_go().get_typelist())
-        self.accounts.go(data=json.dumps(data), type="comptes").check_response()
+        self.accounts.go(data=json.dumps(data), type='comptes').check_response()
         for key in self.page.get_keys():
             for a in self.page.iter_accounts(key=key):
                 yield a
+
         # Next, get saving accounts
         numbers = self.page.get_numbers()
-        for key in self.accounts.go(data=json.dumps({}), type="epargne").get_keys():
+        for key in self.accounts.go(data=json.dumps({}), type='epargne').get_keys():
             for a in self.page.iter_products(key=key, numbers=numbers):
                 yield a
 
@@ -99,31 +104,56 @@ class CmsoParBrowser(LoginBrowser):
     def iter_history(self, account):
         if account.type is Account.TYPE_LOAN:
             return iter([])
+
         if account.type == Account.TYPE_LIFE_INSURANCE:
             url = json.loads(self.lifeinsurance.go(accid=account._index).content)['url']
             url = self.location(url).page.get_link(u"opérations")
-            return self.location(url).page.iter_history()
+
+            history = self.location(url).page.iter_history()
+
         elif account.type == Account.TYPE_MARKET:
-            self.location(json.loads(self.market.go(data=json.dumps({"place": \
-                          "SITUATION_PORTEFEUILLE"})).content)['urlSSO'])
+            self.location(json.loads(self.market.go(data=json.dumps({'place': 'SITUATION_PORTEFEUILLE'})).content)['urlSSO'])
             self.session.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-            if not self.market.go(website=self.website, action="historique").get_list(account.label):
+
+            if not self.market.go(website=self.website, action='historique').get_list(account.label):
                 return iter([])
+
             if not self.page.get_full_list():
                 return iter([])
+
             # Display code ISIN
-            history = self.location("%s?reload=oui&convertirCode=oui" % self.url).page.iter_history()
+            history = self.location('%s?reload=oui&convertirCode=oui' % self.url).page.iter_history()
             self.session.headers['Content-Type'] = 'application/json'
+
             return history
-        return self.history.go(data=json.dumps({"index": account._index}), \
-               page="detailcompte").iter_history()
+
+        history = []
+
+        if account.type == Account.TYPE_CHECKING:
+            # Get one year
+            for number in ['UN', 'DEUX', 'TROIS', 'QUATRE', 'CINQ', 'SIX', 'SEPT', 'HUIT', 'NEUF', 'DIX', 'ONZE', 'DOUZE']:
+                for transaction in self.history.go(data=json.dumps({'index': account._index, \
+                                                                    'filtreOperationsComptabilisees': 'MOIS_MOINS_%s' % number}), \
+                                                   page='detailcompte').iter_history():
+                    history.append(transaction)
+
+        # Get one month
+        for transaction in self.history.go(data=json.dumps({'index': account._index}), page='detailcompte').iter_history():
+            if (transaction.date.month == datetime.datetime.now().month and account.type == Account.TYPE_CHECKING) or \
+               account.type != Account.TYPE_CHECKING:
+                history.append(transaction)
+
+        return history
 
     @need_login
     def iter_coming(self, account):
         if account.type is Account.TYPE_LOAN:
             return iter([])
+
         comings = []
+
         self.history.go(data=json.dumps({"index": account._index}), page="pendingListOperations")
+
         for key in self.page.get_keys():
             for a in self.page.iter_history(key=key):
                 comings.append(a)
@@ -134,10 +164,12 @@ class CmsoParBrowser(LoginBrowser):
         if account.type == Account.TYPE_LIFE_INSURANCE:
             url = json.loads(self.lifeinsurance.go(accid=account._index).content)['url']
             url = self.location(url).page.get_link("supports")
+
             return self.location(url).page.iter_investment()
         elif account.type == Account.TYPE_MARKET:
             self.location(json.loads(self.market.go(data=json.dumps({"place": \
                           "SITUATION_PORTEFEUILLE"})).content)['urlSSO'])
+
             return self.page.iter_investment() if self.market.go(website=self.website, \
                         action="situation").get_list(account.label) else iter([])
         raise NotImplementedError()
