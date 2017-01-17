c82322ba0b7b55dbf22b3f40bf4dffa20dfb8fbf
Baptiste Delpey <bdelpey@budget-insight.com>
2017-01-17 14:20:32 +0100

    handle another change password url

--- modules/bp/browser.py
+++ modules/bp/browser.py
@@ -49,6 +49,7 @@ class BPBrowser(LoginBrowser, StatesMixin):
     init_ident = URL(r'.*authentification/initialiser-identif.ea', Initident)
     check_password = URL(r'.*authentification/verifierMotDePasse-identif.ea',
                          r'/voscomptes/canalXHTML/securite/authentification/verifierPresenceCompteOK-identif.ea',
+                         r'.*//voscomptes/identification/motdepasse.jsp',
                          CheckPassword)
 
     redirect_page = URL(r'.*voscomptes/identification/identification.ea.*',
