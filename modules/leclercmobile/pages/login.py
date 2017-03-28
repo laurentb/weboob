cb99b86e2872812970001c56364902def5de1ec2
Laurent Bachelier <laurent@bachelier.name>
2017-03-28 10:05:29 +0200

    leclercmobile: Remove support for older mechanize versions

--- modules/leclercmobile/pages/login.py
+++ modules/leclercmobile/pages/login.py
@@ -18,8 +18,10 @@
 # along with weboob. If not, see <http://www.gnu.org/licenses/>.
 
 import StringIO
+
+from mechanize import TextControl
+
 from weboob.deprecated.browser import Page
-from weboob.deprecated.mech import ClientForm
 
 
 class LoginPage(Page):
@@ -30,7 +32,7 @@ class LoginPage(Page):
         form = list(self.browser.forms())[0]
         self.browser.select_form("aspnetForm")
         self.browser.set_all_readonly(False)
-        self.browser.controls.append(ClientForm.TextControl('text', '__ASYNCPOST', {'value': "true"}))
+        self.browser.controls.append(TextControl('text', '__ASYNCPOST', {'value': "true"}))
         self.browser['__EVENTTARGET'] = "ctl00$cMain$lnkValider"
         self.browser['ctl00$cMain$ascSaisieMsIsdn$txtMsIsdn'] = login.encode('iso-8859-1')
         self.browser['ctl00$cMain$txtMdp'] = password.encode('iso-8859-1')
@@ -60,7 +62,7 @@ class LoginPage(Page):
         self.document.write(string)
         controlvalue = string.getvalue().split('__EVENTVALIDATION')[1].split('|')[1]
         state = string.getvalue().split('__VIEWSTATE')[1].split('|')[1]
-        self.browser.controls.append(ClientForm.TextControl('text', 'ctl00$objScriptManager', {'value': "ctl00$ascAttente$panelAttente|ctl00$ascAttente$timerAttente"}))
+        self.browser.controls.append(TextControl('text', 'ctl00$objScriptManager', {'value': "ctl00$ascAttente$panelAttente|ctl00$ascAttente$timerAttente"}))
         self.browser['__VIEWSTATE'] = state
         self.browser['__EVENTTARGET'] = "ctl00$ascAttente$timerAttente"
         self.browser['__EVENTVALIDATION'] = controlvalue
