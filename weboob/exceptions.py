ac0ffc614942a8f011f27d1c8c0918e436268de9
Théo Dorée <tdoree@budget-insight.com>
2017-09-04 11:26:20 +0200

    [weboob] Captcha handling improvement, captcha type added

--- weboob/exceptions.py
+++ weboob/exceptions.py
@@ -46,10 +46,12 @@ class CaptchaQuestion(Exception):
     """
     Handles captcha
     """
-    def __init__(self, website_key, website_url):
+    def __init__(self, website_key=None, website_url=None, image_url=None, type=None):
         super(CaptchaQuestion, self).__init__("The site requires solving a captcha")
         self.website_key = website_key
         self.website_url = website_url
+        self.type = type
+        self.image_url = image_url
 
 
 class BrowserHTTPNotFound(BrowserUnavailable):
