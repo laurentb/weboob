b6d433097f85b56913a1b66d34c04ed82fc6f280
Edouard Lambert <elambert@budget-insight.com>
2017-09-13 11:15:53 +0200

    [banquepopulaire] fixing xpath to get currency on cards

--- modules/banquepopulaire/pages.py
+++ modules/banquepopulaire/pages.py
@@ -564,7 +564,7 @@ class CardsPage(LoggedPage, MyHTMLPage):
                 currency = Account.get_currency(m.group(1))
 
         if currency is None:
-            currency = Account.get_currency(CleanText('//td[@id="tbl1_0_5_Cell"]/span')(self.doc))
+            currency = Account.get_currency(CleanText('//td[@id="tbl1_0_5_Cell"]//span')(self.doc))
 
         for tr in self.doc.xpath('//table[@id="tbl1"]/tbody/tr'):
             cols = tr.xpath('./td')
