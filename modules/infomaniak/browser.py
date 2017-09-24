49e1085cd561bbf2ae9e008079792223c5473bce
Vincent A <dev@indigo.re>
2017-09-24 16:16:44 +0200

    new bill module: infomaniak

---
+++ modules/infomaniak/browser.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2017      Vincent A
+#
+# This file is part of weboob.
+#
+# weboob is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# weboob is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with weboob. If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+from datetime import date
+
+from weboob.browser import LoginBrowser, URL, need_login
+from weboob.exceptions import BrowserIncorrectPassword
+
+from .pages import LoginPage, SubscriptionsPage, DocumentsPage
+
+
+class InfomaniakBrowser(LoginBrowser):
+    BASEURL = 'https://manager.infomaniak.com'
+
+    login = URL(r'https://login.infomaniak.com/login', LoginPage)
+    profile = URL(r'/v3/api/profile/me', SubscriptionsPage)
+    documents = URL(r'/admin/facturation/operations/satable_load.php', DocumentsPage)
+
+    def do_login(self):
+        self.login.go()
+        self.page.login(self.username, self.password)
+
+        if self.login.is_here():
+            raise BrowserIncorrectPassword()
+
+    @need_login
+    def iter_subscription(self):
+        self.profile.go()
+        return [self.page.get_subscription()]
+
+    @need_login
+    def iter_documents(self, subscription):
+        for offset in range(0, 400, 20):
+            data = {
+                'iOffset': offset,
+                'iCount': 20,
+                'sSort': 'none',
+                'iCodeService': '0',
+                'dStart_toolbar_select': 'allOperations',
+                'dStart': '2000-01-01',
+                'dEnd': date.today().strftime('%Y-%m-%d'),
+                'cb_collections_card': '1',
+                'cb_collections_post': '1',
+                'cb_collections_paypal': '1',
+                'cb_collections_iban': '1',
+                'cb_collections_bvr': '1',
+                'cb_collections_prepaid': '1',
+                'cb_collections_cash': '1',
+            }
+            self.documents.go(data=data)
+            some = False
+            for doc in self.page.iter_documents(subid=subscription.id):
+                some = True
+                yield doc
+            if not some:
+                break
+        else:
+            assert False, 'are there that many bills?'
---
+++ modules/infomaniak/pages.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2017      Vincent A
+#
+# This file is part of weboob.
+#
+# weboob is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# weboob is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with weboob. If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+from datetime import datetime
+
+from weboob.browser.pages import HTMLPage, LoggedPage, JsonPage
+from weboob.browser.elements import ItemElement, method, DictElement
+from weboob.browser.filters.standard import (
+    CleanDecimal, Env, Regexp, Format, Currency, Field, Eval,
+)
+from weboob.browser.filters.json import Dict
+from weboob.capabilities.bill import Bill, Subscription
+from weboob.tools.compat import urljoin
+
+
+class LoginPage(HTMLPage):
+    def login(self, login, password):
+        form = self.get_form(nr=0)
+        form['login'] = login
+        form['password'] = password
+        form.submit()
+
+
+class SubscriptionsPage(LoggedPage, JsonPage):
+    @method
+    class get_subscription(ItemElement):
+        klass = Subscription
+
+        obj_id = Eval(str, Dict('data/user/current_account_id'))
+        obj_subscriber = Dict('data/user/display_name')
+        obj_label = Dict('data/user/display_name')
+
+
+class DocumentsPage(LoggedPage, JsonPage):
+    @method
+    class iter_documents(DictElement):
+        item_xpath = 'aLines'
+
+        class item(ItemElement):
+            klass = Bill
+
+            def obj_url(self):
+                return urljoin(self.page.url, Regexp(Dict('sOperation'), r'&quot;(/.*\.pdf)')(self))
+
+            _num = Regexp(Field('url'), r'facture_(\d+).pdf')
+
+            obj_id = Format('%s_%s', Env('subid'), _num)
+            obj_date = Eval(datetime.fromtimestamp, Dict('sTimestamp'))
+            obj_label = Format('Facture %s', _num)
+            obj_price = CleanDecimal(Dict('fMontant'))
+            obj_currency = Currency(Dict('sMontant'))
+            obj_type = 'bill'
+            obj_format = 'pdf'
