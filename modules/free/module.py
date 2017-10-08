71da1ee44ab4fe27737b22b77bfaefe56399eaae
Romain Bignon <romain@symlink.me>
2017-10-08 11:35:49 +0200

    [free] bump version to 1.4

--- modules/free/module.py
+++ modules/free/module.py
@@ -35,7 +35,7 @@ class FreeModule(Module, CapDocument):
     MAINTAINER = u'Edouard Lambert'
     EMAIL = 'elambert@budget-insight.com'
     LICENSE = 'LGPLv3+'
-    VERSION = '1.3'
+    VERSION = '1.4'
     CONFIG = BackendConfig(Value('login', label='Identifiant'),
                        ValueBackendPassword('password', label='Mot de passe'))
 
