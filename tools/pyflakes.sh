a995e20b833e8816aa905b690f00e3ceb5b67191
Vincent A <dev@indigo.re>
2017-10-21 20:14:33 +0200

    tools/pyflakes: don't exit at the first error encountered

--- tools/pyflakes.sh
+++ tools/pyflakes.sh
@@ -1,12 +1,7 @@
 #!/bin/bash -u
 . "$(dirname $0)/common.sh"
 
-find_cmd () {
-    for cmd in $@; do
-        which $cmd 2>/dev/null && return
-    done
-    return 1
-}
+err=0
 
 PY3MODS=./tools/py3-compatible.modules
 
@@ -18,22 +13,22 @@ MODULE_FILES3=$(printf "%s\n" $MODULE_FILES|grep -F -f $PY3MODS)
 PYFILES=$(git ls-files | grep '^scripts\|\.py$'|grep -v boilerplate_data|grep -v '^modules'|grep -v '^contrib')
 PYFILES3="$(printf "%s\n" $PYFILES | grep -v /deprecated/) $MODULE_FILES3"
 PYFILES="$PYFILES $MODULE_FILES"
-grep -n 'class [^( ]\+:$' ${PYFILES} && echo 'Error: old class style found, always inherit object' && exit 3
-grep -n $'\t\|\s$' ${PYFILES} && echo 'Error: tabs or trailing whitespace found, remove them' && exit 4
-grep -Fn '.setlocale' ${PYFILES} && echo 'Error: do not use setlocale' && exit 5
-grep -Fn '__future__ import with_statement' ${PYFILES} && echo 'Error: with_statement useless as we do not support Python 2.5' &&  exit 6
-grep -nE '^[[:space:]]+except [[:alnum:] ]+,[[:alnum:] ]+' ${PYFILES} && echo 'Error: use new "as" way of naming exceptions' && exit 7
-grep -nE "^ *print " ${PYFILES} && echo 'Error: Use the print function' && exit 8
-grep -Fn ".has_key" ${PYFILES} && echo 'Error: Deprecated, use operator "in"' && exit 9
-grep -Fn "os.isatty" ${PYFILES} && echo 'Error: Use stream.isatty() instead of os.isatty(stream.fileno())' && exit 10
-grep -Fn "raise StopIteration" ${PYFILES} && echo 'Error: PEP 479' && exit 11
+grep -n 'class [^( ]\+:$' ${PYFILES} && echo 'Error: old class style found, always inherit object' && err=3
+grep -n $'\t\|\s$' ${PYFILES} && echo 'Error: tabs or trailing whitespace found, remove them' && err=4
+grep -Fn '.setlocale' ${PYFILES} && echo 'Error: do not use setlocale' && err=5
+grep -Fn '__future__ import with_statement' ${PYFILES} && echo 'Error: with_statement useless as we do not support Python 2.5' && err=6
+grep -nE '^[[:space:]]+except [[:alnum:] ]+,[[:alnum:] ]+' ${PYFILES} && echo 'Error: use new "as" way of naming exceptions' && err=7
+grep -nE "^ *print " ${PYFILES} && echo 'Error: Use the print function' && err=8
+grep -Fn ".has_key" ${PYFILES} && echo 'Error: Deprecated, use operator "in"' && err=9
+grep -Fn "os.isatty" ${PYFILES} && echo 'Error: Use stream.isatty() instead of os.isatty(stream.fileno())' && err=10
+grep -Fn "raise StopIteration" ${PYFILES} && echo 'Error: PEP 479' && err=11
 
-grep -nE "\.iter(keys|values|items)\(\)" ${PYFILES3} | grep -Fv "six.iter" && echo 'Error: iterkeys/itervalues/iteritems is forbidden' && exit 12
+grep -nE "\.iter(keys|values|items)\(\)" ${PYFILES3} | grep -Fv "six.iter" && echo 'Error: iterkeys/itervalues/iteritems is forbidden' && err=12
 
-grep -nE "^ *print(\(| )" ${MODULE_FILES} && echo 'Error: Use of print in modules is forbidden, use logger instead' && exit 20
-grep -n xrange ${MODULE_FILES3} && echo 'Error: xrange is forbidden' && exit 21
-grep -nE "from (urllib|urlparse) import" ${MODULE_FILES3} && echo 'Error: python2 urllib is forbidden' && exit 22
-grep -nE "^import (urllib|urlparse)$" ${MODULE_FILES3} && echo 'Error: python2 urllib is forbidden' && exit 22
+grep -nE "^ *print(\(| )" ${MODULE_FILES} && echo 'Error: Use of print in modules is forbidden, use logger instead' && err=20
+grep -n xrange ${MODULE_FILES3} && echo 'Error: xrange is forbidden' && err=21
+grep -nE "from (urllib|urlparse) import" ${MODULE_FILES3} && echo 'Error: python2 urllib is forbidden' && err=22
+grep -nE "^import (urllib|urlparse)$" ${MODULE_FILES3} && echo 'Error: python2 urllib is forbidden' && err=22
 
 if [ ${VER} -eq 2 ]
 then
@@ -45,9 +40,11 @@ then
       OPT2=
   else
       echo "flake8 or pyflakes for python2 not found"
-      exit 1
+      err=1
   fi
-  $PYTHON2 -m ${FLAKER2} ${OPT2} ${PYFILES} || exit 32
+  if [ ${err} -ne 1 ]; then
+    $PYTHON2 -m ${FLAKER2} ${OPT2} ${PYFILES} || err=32
+  fi
 fi
 
 if [ ${VER} -eq 3 ]
@@ -60,9 +57,11 @@ then
       OPT3=
   else
       echo "flake8 or pyflakes for python3 not found"
-      exit 1
+      err=1
   fi
-  $PYTHON3 -m ${FLAKER3} ${OPT3} ${PYFILES3} || exit 33
+  if [ ${err} -ne 1 ]; then
+    $PYTHON3 -m ${FLAKER3} ${OPT3} ${PYFILES3} || exit 33
+  fi
 fi
 
-exit 0
+exit $err
