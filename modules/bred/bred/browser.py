82271b71a2a21af99a57619d92431e4c24480a03
Vincent Ardisson <vardisson@budget-insight.com>
2017-10-25 14:32:33 +0200

    [bred] raise again history offset limit

--- modules/bred/bred/browser.py
+++ modules/bred/bred/browser.py
@@ -252,5 +252,5 @@ class BredBrowser(DomainBrowser):
             next_page = bool(transactions)
             offset += 50
 
-            assert offset < 10000, 'the site may be doing an infinite loop'
+            assert offset < 30000, 'the site may be doing an infinite loop'
 
