f68d95c222b438ed3c3b9dd7012ab78841fccffa
Vincent Ardisson <vardisson@budget-insight.com>
2017-11-02 11:13:42 +0100

    boilerplate: remove useless unicode prefixes

--- tools/boilerplate_data/cap_module.py
+++ tools/boilerplate_data/cap_module.py
@@ -13,8 +13,8 @@ __all__ = ['${r.classname}Module']
 
 class ${r.classname}Module(Module, ${r.capname}):
     NAME = '${r.name}'
-    DESCRIPTION = u'${r.name} website'
-    MAINTAINER = u'${r.author}'
+    DESCRIPTION = '${r.name} website'
+    MAINTAINER = '${r.author}'
     EMAIL = '${r.email}'
     LICENSE = 'AGPLv3+'
     VERSION = '${r.version}'
