58c6efdfb7feb6d4041a111e973fe3ca869af14c
Fong NGO <fngo@budget-insight.com>
2018-02-08 19:18:18 +0100

    [societegenerale] add type for compte Alterna

--- modules/societegenerale/pages/accounts_list.py
+++ modules/societegenerale/pages/accounts_list.py
@@ -52,6 +52,7 @@ class AccountsList(LoggedPage, BasePage):
              u'Compte Epargne':      Account.TYPE_SAVINGS,
              u'Compte Sur Livret':   Account.TYPE_SAVINGS,
              u'Compte Titres':       Account.TYPE_MARKET,
+             u'Compte Alterna':      Account.TYPE_REVOLVING_CREDIT,
              u'Crédit':              Account.TYPE_LOAN,
              u'Ldd':                 Account.TYPE_SAVINGS,
              u'Livret':              Account.TYPE_SAVINGS,
