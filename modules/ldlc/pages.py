07c320bcf9b8cd70b3e531d9bee8c806a846015b
Tony Malto <tmalto.bi@gmail.com>
2018-02-27 12:13:13 +0100

    [ldlc] fix login and label/subscriber fetching

--- modules/ldlc/pages.py
+++ modules/ldlc/pages.py
@@ -18,7 +18,7 @@
 # along with weboob. If not, see <http://www.gnu.org/licenses/>.
 
 from weboob.browser.pages import HTMLPage, LoggedPage
-from weboob.browser.filters.standard import CleanDecimal, CleanText, Env, Format
+from weboob.browser.filters.standard import CleanDecimal, CleanText, Env, Format, QueryValue
 from weboob.browser.elements import ListElement, ItemElement, method
 from weboob.browser.filters.html import Attr
 from weboob.capabilities.bill import Bill, Subscription
@@ -31,19 +31,23 @@ class HomePage(LoggedPage, HTMLPage):
         class item(ItemElement):
             klass = Subscription
 
-            obj_subscriber = CleanText('.//div[@id="divlblTitleFirstNameLastName"]/span')
+            obj_subscriber = CleanText('.//div[@id="divlblTitleFirstNameLastName"]//span')
             obj_id = CleanText('.//span[2]')
-            obj_label = CleanText('.//div[@id="divlblTitleFirstNameLastName"]/span')
+            obj_label = CleanText('.//div[@id="divlblTitleFirstNameLastName"]//span')
 
 
 class LoginPage(HTMLPage):
     def login(self, username, password):
         form = self.get_form(xpath='//form[@id="aspnetForm"]')
-        form["ctl00$ctl00$cphMainContent$cphMainContent$txbMail"] = username
-        form["ctl00$ctl00$cphMainContent$cphMainContent$txbPassword"] = password
-        form["__EVENTTARGET"] = "ctl00$ctl00$cphMainContent$cphMainContent$butConnexion"
-        form["ctl00_ctl00_actScriptManager_HiddenField"] = ";;LIBLDLC:fr-FR:2a6faad4-8912-415f-8586-e1088a6418ed:e5e73cb5;AjaxControlToolkit, Version=3.5.40412.0, Culture=neutral, PublicKeyToken=28f01b0e84b6d53e:fr-FR:f0f17dea-2bbf-45d7-9f07-8361cd7f1424:5546a2b:475a4ef5:d2e10b12;LIBLDLC, Version=1.1.3.0, Culture=neutral, PublicKeyToken=null:fr-FR:2a6faad4-8912-415f-8586-e1088a6418ed::::70413c6f;AjaxControlToolkit, Version=3.5.40412.0, Culture=neutral, PublicKeyToken=28f01b0e84b6d53e:fr-FR:f0f17dea-2bbf-45d7-9f07-8361cd7f1424:effe2a26:751cdd15:dfad98a5:1d3ed089:497ef277:a43b07eb:3cf12cf1"
+        form["ctl00$cphMainContent$txbMail"] = username
+        form["ctl00$cphMainContent$txbPassword"] = password
+        form["__EVENTTARGET"] = "ctl00$cphMainContent$butConnexion"
+        form["ctl00_actScriptManager_HiddenField"] = self.get_ctl00_actScriptManager_HiddenField()
         form.submit()
+
+    def get_ctl00_actScriptManager_HiddenField(self):
+        param = QueryValue(Attr('//script[contains(@src, "js/CombineScriptsHandler.ashx?")]', 'src'), "_TSM_CombinedScripts_")(self.doc)
+        return param
 
 
 class BillsPage(LoggedPage, HTMLPage):
@@ -70,4 +74,3 @@ class BillsPage(LoggedPage, HTMLPage):
 
             def condition(self):
                 return CleanText().filter(self.el.xpath('.//td')[-1]) != "" and len(self.el.xpath('./td[@class="center" or @class="center pdf"]/a/@href')) == 1
-
