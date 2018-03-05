53dc29e28c1392ff347ef27b4fa0b7f2894ad9b6
Vincent A <dev@indigo.re>
2018-03-05 22:20:13 +0100

    captcha: dedicated exceptions per type

--- weboob/exceptions.py
+++ weboob/exceptions.py
@@ -43,14 +43,43 @@ class BrowserQuestion(BrowserIncorrectPassword):
 
 
 class CaptchaQuestion(Exception):
-    """
-    Handles captcha
-    """
+    """Site requires solving a CAPTCHA (base class)"""
+    # could be improved to pass the name of the backendconfig key
+
     def __init__(self, type=None, **kwargs):
         super(CaptchaQuestion, self).__init__("The site requires solving a captcha")
         self.type = type
         for key, value in kwargs.items():
             setattr(self, key, value)
+
+
+class ImageCaptchaQuestion(CaptchaQuestion):
+    type = 'image_captcha'
+
+    image_data = None
+
+    def __init__(self, image_data):
+        super(ImageCaptchaQuestion, self).__init__(self.type, image_data=image_data)
+
+
+class NocaptchaQuestion(CaptchaQuestion):
+    type = 'nocaptcha'
+
+    website_key = None
+    website_url = None
+
+    def __init__(self, website_key, website_url):
+        super(NocaptchaQuestion, self).__init__(self.type, website_key=website_key, website_url=website_url)
+
+
+class RecaptchaQuestion(CaptchaQuestion):
+    type = 'g_recaptcha'
+
+    website_key = None
+    website_url = None
+
+    def __init__(self, website_key, website_url):
+        super(RecaptchaQuestion, self).__init__(self.type, website_key=website_key, website_url=website_url)
 
 
 class BrowserHTTPNotFound(BrowserUnavailable):
