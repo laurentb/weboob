57da283afb55083ab00001e6b17f595a38f903c6
Sylvie Ye <sye@budget-insight.com>
2018-03-08 11:35:27 +0100

    [societegenerale] change transfer execution date offset
    Change transfer execution date offset from 2 to 3 days.
    Bank can be closed 'saturday, sunday, monday'
    or 'sunday, monday, tuesday' if tuesday is a plublic holiday.
    
    Closes: 559621@redmine

--- modules/societegenerale/pages/transfer.py
+++ modules/societegenerale/pages/transfer.py
@@ -245,7 +245,7 @@ class TransferPage(LoggedPage, BasePage, PasswordPage):
             raise TransferError('data consistency failed, %s changed from %s to %s' % ('amount', transfer.amount, amount))
         if transfer.label not in label:
             raise TransferError('data consistency failed, %s changed from %s to %s' % ('label', transfer.label, label))
-        if not (transfer.exec_date <= exec_date <= transfer.exec_date + timedelta(days=2)):
+        if not (transfer.exec_date <= exec_date <= transfer.exec_date + timedelta(days=3)):
             raise TransferError('data consistency failed, %s changed from %s to %s' % ('exec_date', transfer.exec_date, exec_date))
 
     def create_transfer(self, account, recipient, transfer):
