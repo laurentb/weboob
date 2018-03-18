6e69fbbe9e116faee0d494087e0d226966b4e8b1
Phyks (Lucas Verney) <phyks@phyks.me>
2018-03-18 12:49:37 +0100

    [logicimmo] Use the common HousingTest class for tests

--- modules/logicimmo/test.py
+++ modules/logicimmo/test.py
@@ -17,63 +17,36 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with weboob. If not, see <http://www.gnu.org/licenses/>.
 
-import itertools
 from weboob.capabilities.housing import Query, POSTS_TYPES, ADVERT_TYPES
 from weboob.tools.test import BackendTest
+from weboob.tools.capabilities.housing.housing_test import HousingTest
 
 
-class LogicimmoTest(BackendTest):
+class LogicimmoTest(BackendTest, HousingTest):
     MODULE = 'logicimmo'
 
-    def check_housing_lists(self, query):
-        results = list(itertools.islice(
-            self.backend.search_housings(query),
-            20
-        ))
-        self.assertTrue(len(results) > 0)
-
-        self.assertTrue(any(x.photos for x in results))
-        self.assertTrue(any(x.rooms for x in results))
-        self.assertTrue(any(x.text for x in results))
-
-        for x in results:
-            self.assertIn(x.house_type, [
-                str(y) for y in query.house_types
-            ])
-            for photo in x.photos:
-                self.assertRegexpMatches(photo.url, r'^http(s?)://')
-            self.assertTrue(x.date)
-            self.assertTrue(x.location)
-            self.assertTrue(x.area)
-            self.assertTrue(x.cost)
-            self.assertTrue(x.currency)
-            self.assertTrue(x.title)
-            self.assertEqual(x.type, query.type)
-            self.assertTrue(x.id)
-            self.assertTrue(x.url)
-            self.assertIn(x.advert_type, query.advert_types)
-
-        return results
-
-    def check_single_housing(self, housing, advert_type):
-        self.assertTrue(housing.id)
-        self.assertTrue(housing.type)
-        self.assertEqual(housing.advert_type, advert_type)
-        self.assertTrue(housing.house_type)
-        self.assertTrue(housing.title)
-        self.assertTrue(housing.cost)
-        self.assertTrue(housing.currency)
-        self.assertTrue(housing.area)
-        self.assertTrue(housing.date)
-        self.assertTrue(housing.location)
-        self.assertTrue(housing.rooms)
-        self.assertTrue(housing.text)
-        self.assertTrue(housing.url)
-        self.assertTrue(housing.photos)
-        for photo in housing.photos:
-            self.assertRegexpMatches(photo.url, r'^http(s?)://')
-        self.assertTrue(housing.details.keys())
-        # No tests for DPE, GES, rooms
+    FIELDS_ALL_HOUSINGS_LIST = [
+        "id", "type", "advert_type", "house_type", "url", "title", "area",
+        "cost", "currency", "utilities", "date", "location", "text",
+        "details"
+    ]
+    FIELDS_ANY_HOUSINGS_LIST = [
+        "photos",
+        "rooms"
+    ]
+    FIELDS_ALL_SINGLE_HOUSING = [
+        "id", "url", "type", "advert_type", "house_type", "title", "area",
+        "cost", "currency", "utilities", "date", "location", "text",
+        "phone", "details"
+    ]
+    FIELDS_ANY_SINGLE_HOUSING = [
+        "photos",
+        "station",
+        "rooms",
+        "phone",
+        "DPE",
+        "GES"
+    ]
 
     def test_logicimmo_rent(self):
         query = Query()
@@ -86,12 +59,7 @@ class LogicimmoTest(BackendTest):
             query.cities.append(city)
             if len(query.cities) == 3:
                 break
-
-        results = self.check_housing_lists(query)
-
-        housing = self.backend.get_housing(results[0].id)
-        self.backend.fillobj(housing, 'phone')
-        self.check_single_housing(housing, results[0].advert_type)
+        self.check_against_query(query)
 
     def test_logicimmo_sale(self):
         query = Query()
@@ -103,12 +71,7 @@ class LogicimmoTest(BackendTest):
             query.cities.append(city)
             if len(query.cities) == 3:
                 break
-
-        results = self.check_housing_lists(query)
-
-        housing = self.backend.get_housing(results[0].id)
-        self.backend.fillobj(housing, 'phone')
-        self.check_single_housing(housing, results[0].advert_type)
+        self.check_against_query(query)
 
     def test_logicimmo_furnished_rent(self):
         query = Query()
@@ -121,12 +84,7 @@ class LogicimmoTest(BackendTest):
             query.cities.append(city)
             if len(query.cities) == 3:
                 break
-
-        results = self.check_housing_lists(query)
-
-        housing = self.backend.get_housing(results[0].id)
-        self.backend.fillobj(housing, 'phone')
-        self.check_single_housing(housing, results[0].advert_type)
+        self.check_against_query(query)
 
     def test_logicimmo_viager(self):
         query = Query()
@@ -137,12 +95,7 @@ class LogicimmoTest(BackendTest):
             query.cities.append(city)
             if len(query.cities) == 3:
                 break
-
-        results = self.check_housing_lists(query)
-
-        housing = self.backend.get_housing(results[0].id)
-        self.backend.fillobj(housing, 'phone')
-        self.check_single_housing(housing, results[0].advert_type)
+        self.check_against_query(query)
 
     def test_logicimmo_personal(self):
         query = Query()
