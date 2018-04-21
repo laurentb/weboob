fb5461f9e1d5be64023307ffca9057264c3f9764
Laurent Bachelier <laurent@bachelier.name>
2018-04-21 12:30:19 +0200

    weboob_lint: More readable output

--- tools/weboob_lint.py
+++ tools/weboob_lint.py
@@ -1,44 +1,46 @@
 #!/usr/bin/env python3
 from __future__ import print_function
 
+import logging
+import os
+import subprocess
+import sys
+
+from weboob.core import Weboob
+
 # Hint: use this script with file:///path/to/local/modules/ in sources.list
 # if you want to correctly check all modules.
 
-from weboob.core import Weboob
-import os
-import sys
-import subprocess
+
+logging.basicConfig()
 
 weboob = Weboob()
 weboob.modules_loader.load_all()
 
-backends_without_tests = []
-backends_without_icons = []
-backends_using_deprecated = []
+modules_without_tests = []
+modules_without_icons = []
+modules_using_deprecated = []
 
-for name, backend in weboob.modules_loader.loaded.items():
-    path = backend.package.__path__[0]
+for name, module in weboob.modules_loader.loaded.items():
+    path = module.package.__path__[0]
     if not os.path.exists(os.path.join(path, 'test.py')):
-        backends_without_tests.append(name)
+        modules_without_tests.append(name)
     if not os.path.exists(os.path.join(path, 'favicon.png')) and \
        not os.path.exists(os.path.join(weboob.repositories.icons_dir, '%s.png' % name)) and \
-       not backend.icon:
-        backends_without_icons.append(name)
+       not module.icon:
+        modules_without_icons.append(name)
 
     if subprocess.call(['grep', '-q', '-r', 'weboob.deprecated.browser', path]) == 0:
-        backends_using_deprecated.append(name)
+        modules_using_deprecated.append(name)
 
 
-if backends_without_tests:
-    backends_without_tests.sort()
-    print('Modules without tests: %s' % backends_without_tests)
-if backends_without_icons:
-    backends_without_icons.sort()
-    print('Modules without icons: %s' % backends_without_icons)
-if backends_using_deprecated:
-    backends_using_deprecated.sort()
-    print('Modules using deprecated Browser 1: %s' % backends_using_deprecated)
+if modules_without_tests:
+    print('\nModules without tests: %s' % ', '.join(sorted(modules_without_tests)))
+if modules_without_icons:
+    print('\nModules without icons: %s' % ', '.join(sorted(modules_without_icons)))
+if modules_using_deprecated:
+    print('\nModules using deprecated Browser 1: %s' % ', '.join(sorted(modules_using_deprecated)))
 
 
-if backends_without_tests or backends_without_icons:
+if modules_without_tests or modules_without_icons or modules_using_deprecated:
     sys.exit(1)
