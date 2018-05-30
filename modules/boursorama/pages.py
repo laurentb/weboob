294a5420ef27080e8d780315abc0a273fbd0f645
Quentin Defenouillere <quentin.defenouillere@budget-insight.com>
2018-05-30 17:37:39 +0200

    [boursorama] Corrected indentation pages.py line 183
    Small change: corrected indentation on line 183,
    there were 7 spaces instead of 8.

--- modules/boursorama/pages.py
+++ modules/boursorama/pages.py
@@ -180,7 +180,7 @@ class LoginPage(LoginPage, HTMLPage):
 
     def login(self, login, password):
         if not password.isdigit():
-           password = ''.join([c if c.isdigit() else [k for k, v in self.TO_DIGIT.items() if c in v][0] for c in password.lower()])
+            password = ''.join([c if c.isdigit() else [k for k, v in self.TO_DIGIT.items() if c in v][0] for c in password.lower()])
         form = self.get_form()
         keyboard_page = self.browser.keyboard.open()
         vk = BoursoramaVirtKeyboard(keyboard_page)
