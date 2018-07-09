9273904c251b9544d32b53fc79194d768d4e783b
Maxime Gasselin <maxime.gasselin@budget-insight.com>
2018-07-09 19:01:18 +0200

    [creditmutuel] Fix url for decouverte account
    The self.currentSubBank self.url need to be : cmag/decouverte/ and not only
    cmag/.

--- modules/creditmutuel/browser.py
+++ modules/creditmutuel/browser.py
@@ -209,6 +209,8 @@ class CreditMutuelBrowser(LoginBrowser, StatesMixin):
         # the account list and history urls depend on the sub bank of the user
         paths = urlparse(self.url).path.lstrip('/').split('/')
         self.currentSubBank = paths[0] + "/" if paths[0] != "fr" else ""
+        if self.currentSubBank and paths[1] == "decouverte":
+            self.currentSubBank += paths[1] + "/"
         if paths[0] in ["fr", "mabanque"]:
             self.is_new_website = True
 
