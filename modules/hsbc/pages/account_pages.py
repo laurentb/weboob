527be10ccace69498f79d75a849ff8b1bdc6fbb5
Célande Adrien <celande.adrien@budget-insight.com>
2018-07-17 18:52:08 +0200

    [hsbc] add new label account to type
    Closes: 5944@zendesk

--- modules/hsbc/pages/account_pages.py
+++ modules/hsbc/pages/account_pages.py
@@ -124,6 +124,7 @@ class AccountsPage(GenericLandingPage):
                     ('carte ', Account.TYPE_CARD),
                     ('plan assur. innovat.', Account.TYPE_LIFE_INSURANCE),
                     ('hsbc evol pat transf', Account.TYPE_LIFE_INSURANCE),
+                    ('bourse libre', Account.TYPE_MARKET),
                 ]
 
                 def filter(self, label):
