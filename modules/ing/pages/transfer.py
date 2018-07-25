9f2f7bb5ebb936b3765edd14edcf53192077775e
Sylvie Ye <sye@budget-insight.com>
2018-07-25 18:42:15 +0200

    [ing] update transfer account balance xpath
    Transfer origin account balance can be negative
    
    Closes: 16355@sibi

--- modules/ing/pages/transfer.py
+++ modules/ing/pages/transfer.py
@@ -182,8 +182,9 @@ class TransferPage(LoggedPage, HTMLPage):
 
         assert origin.label == CleanText('//div[@id="transferSummary"]/div[has-class("debit")]//span[@class="title"]')(self.doc)
 
+        # check origin account balance
         origin_balance = CleanDecimal('//div[@id="transferSummary"]/div[has-class("debit")]\
-        //label[@class="digits positive"]', replace_dots=True)(self.doc)
+                                       //label[has-class("digits")]', replace_dots=True)(self.doc)
         assert (origin_balance == origin.balance) or (origin_balance == origin._estimated_balance)
         t.account_balance = origin.balance
         t.account_label = origin.label
