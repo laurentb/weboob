5a01201bb130506ac58b8e2b116fd64de0bb663b
Maxime Gasselin <maxime.gasselin@budget-insight.com>
2018-07-30 14:35:49 +0200

    [cragr] Type DAV NANTI accounts
    Based on the transactions from several accounts with "DAV NANTI" label
    we type these accounts to saving.
    closes: 6335@zendesk

--- modules/cragr/web/pages.py
+++ modules/cragr/web/pages.py
@@ -203,6 +203,7 @@ class AccountsPage(MyLoggedPage, BasePage):
              u'CCOU':       Account.TYPE_CHECKING, # pro
              u'AUTO ENTRP': Account.TYPE_CHECKING, # pro
              u'EKO' :       Account.TYPE_CHECKING,
+             u'DAV NANTI':  Account.TYPE_SAVINGS,
              u'LIV A':      Account.TYPE_SAVINGS,
              u'LDD':        Account.TYPE_SAVINGS,
              u'PEL':        Account.TYPE_SAVINGS,
