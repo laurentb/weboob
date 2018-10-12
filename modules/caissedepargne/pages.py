b95b11ac0adc68ac36e4b40172af2d4feb51f5ac
Sylvie Ye <sye@budget-insight.com>
2018-10-12 17:36:19 +0200

    [caissedepargne] rate of credits may be not available
    set default=NotAvailable
    
    Closes: 25916@sibi

--- modules/caissedepargne/pages.py
+++ modules/caissedepargne/pages.py
@@ -473,7 +473,7 @@ class IndexPage(LoggedPage, HTMLPage):
                 obj_label = Env('label')
                 obj_type = Loan.TYPE_LOAN
                 obj_total_amount = MyDecimal(MyTableCell("total_amount"))
-                obj_rate = MyDecimal(MyTableCell("rate"))
+                obj_rate = MyDecimal(MyTableCell("rate", default=NotAvailable), default=NotAvailable)
                 obj_balance = MyDecimal(MyTableCell("balance"))
                 obj_currency = Currency(MyTableCell("balance"))
                 obj_last_payment_date = Date(CleanText(MyTableCell("last_payment_date")))
