e604deb6dd320a0d4efac0388e47c1f83fdb3f23
Sylvie Ye <sye@budget-insight.com>
2018-10-30 16:00:54 +0100

    [bnp] add new wrongpass code and action needed code
    wrongpass code 7 is "Vous ne pouvez pas encore vous connecter car votre code secret est en cours d'envoi"
    action needed code 50 is "Votre mot de passe temporaire a expiré. Veuillez effectuer une nouvelle demande."
    
    error code 207 is "Ce service est momentanément indisponible. Veuillez renouveler votre demande ultérieurement."
    
    Closes: 27655@sibi 27257@sibi 27481@sibi 27483@sibi 27478@sibi

--- modules/bnporc/pp/pages.py
+++ modules/bnporc/pp/pages.py
@@ -202,14 +202,16 @@ class LoginPage(JsonPage):
             if not msg:
                 msg = self.get('message')
 
-            wrongpass_codes = [201, 21510, 203, 202, 1001]
-            actionNeeded_codes = [21501, 3, 4]
+            wrongpass_codes = [201, 21510, 203, 202, 1001, 7]
+            actionNeeded_codes = [21501, 3, 4, 50]
             if error in wrongpass_codes:
                 raise BrowserIncorrectPassword(msg)
             elif error == 21: # "Ce service est momentanément indisponible. Veuillez renouveler votre demande ultérieurement." -> In reality, account is blocked because of too much wrongpass
                 raise ActionNeeded(u"Compte bloqué")
             elif error in actionNeeded_codes:
                 raise ActionNeeded(msg)
+            elif error == 207:
+                raise BrowserUnavailable(msg)
             else:
                 assert False, 'Unexpected error at login: "%s" (code=%s)' % (msg, error)
 
