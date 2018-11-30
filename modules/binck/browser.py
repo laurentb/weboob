d1469a188f8c8e9196b37d9bbd4707d74142d020
Sylvie Ye <sye@budget-insight.com>
2018-11-30 15:02:16 +0100

    [binck] actionNeeded for frozen or closed accounts

--- modules/binck/browser.py
+++ modules/binck/browser.py
@@ -58,6 +58,11 @@ class BinckBrowser(LoginBrowser):
             error = self.page.get_error()
             if error and 'mot de passe' in error:
                 raise BrowserIncorrectPassword(error)
+            elif error and any(
+                'Votre compte a été bloqué / clôturé' in error,
+                'Votre compte est bloqué, veuillez contacter le Service Clients' in error,
+            ):
+                raise ActionNeeded(error)
             raise AssertionError('Unhandled behavior at login: error is "{}"'.format(error))
 
     @need_login
