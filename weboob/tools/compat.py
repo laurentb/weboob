a8ca60151dca60f35351fd91d24a6425eb6e5848
Vincent Ardisson <vardisson@budget-insight.com>
2018-12-06 19:13:04 +0100

    weboob.tools.compat: handle unicode for unquote* and parse_qs*
    Unicode should be accepted too in python2, and result should be
    unicode-decoded.
    
    u'%C3%A9' should give u'\xe9' not u'\xc3\xa9'
    
    Also add tests.

--- weboob/tools/compat.py
+++ weboob/tools/compat.py
@@ -75,8 +75,8 @@ else:
 
 
 try:
-    from urllib import quote as _quote, quote_plus as _quote_plus, unquote, unquote_plus, urlencode as _urlencode, getproxies
-    from urlparse import urlparse, urlunparse, urljoin, urlsplit, urlunsplit, parse_qsl, parse_qs
+    from urllib import quote as _quote, quote_plus as _quote_plus, unquote as _unquote, unquote_plus as _unquote_plus, urlencode as _urlencode, getproxies
+    from urlparse import urlparse, urlunparse, urljoin, urlsplit, urlunsplit, parse_qsl as _parse_qsl, parse_qs as _parse_qs
 
     def _reencode(s):
         if isinstance(s, unicode):
@@ -98,6 +98,24 @@ try:
         d = [(_reencode(k), _reencode(v)) for k, v in d]
 
         return _urlencode(d, *args, **kwargs)
+
+    def unquote(s):
+        s = _reencode(s)
+        return _unquote(s).decode('utf-8')
+
+    def unquote_plus(s):
+        s = _reencode(s)
+        return _unquote_plus(s).decode('utf-8')
+
+    def parse_qs(s):
+        s = _reencode(s)
+        orig = _parse_qs(s)
+        return {k.decode('utf-8'): [vv.decode('utf-8') for vv in v] for k, v in orig.items()}
+
+    def parse_qsl(s):
+        s = _reencode(s)
+        return [(k.decode('utf-8'), v.decode('utf-8')) for k, v in _parse_qsl(s)]
+
 except ImportError:
     from urllib.parse import (
         urlparse, urlunparse, urlsplit, urlunsplit, urljoin, urlencode,
@@ -111,3 +129,36 @@ def unpickle(pickled_data):
     else:  # Assuming future Python versions will not remove encoding argument
         pyobject = pickle.loads(pickled_data, encoding='UTF-8')
     return pyobject
+
+
+def test_base():
+    assert type(range(3)) != list
+    assert type(u'') == unicode
+    assert type(b'') == bytes
+    assert isinstance(u'', basestring)
+
+
+def test_url():
+    assert quote( 'foo=é&bar=qux ,/%') == u'foo%3D%C3%A9%26bar%3Dqux%20%2C/%25'
+    assert quote(u'foo=é&bar=qux ,/%') == u'foo%3D%C3%A9%26bar%3Dqux%20%2C/%25'
+
+    assert quote_plus( 'foo=é&bar=qux ,/%') == u'foo%3D%C3%A9%26bar%3Dqux+%2C%2F%25'
+    assert quote_plus(u'foo=é&bar=qux ,/%') == u'foo%3D%C3%A9%26bar%3Dqux+%2C%2F%25'
+
+    assert unquote( 'foo%3D%C3%A9%26bar%3Dqux%20%2C/%25') == u'foo=é&bar=qux ,/%'
+    assert unquote(u'foo%3D%C3%A9%26bar%3Dqux%20%2C/%25') == u'foo=é&bar=qux ,/%'
+
+    assert unquote_plus( 'foo%3D%C3%A9%26bar%3Dqux+%2C%2F%25') == u'foo=é&bar=qux ,/%'
+    assert unquote_plus(u'foo%3D%C3%A9%26bar%3Dqux+%2C%2F%25') == u'foo=é&bar=qux ,/%'
+
+    assert urlencode([( 'foo', u'é'), ( 'bar',  'qux ,/%')]) == u'foo=%C3%A9&bar=qux+%2C%2F%25'
+    assert urlencode([(u'foo', u'é'), (u'bar', u'qux ,/%')]) == u'foo=%C3%A9&bar=qux+%2C%2F%25'
+
+    assert urlencode(dict([( 'foo', u'é'), ( 'bar',  'qux ,/%')])) == u'foo=%C3%A9&bar=qux+%2C%2F%25'
+    assert urlencode(dict([(u'foo', u'é'), (u'bar', u'qux ,/%')])) == u'foo=%C3%A9&bar=qux+%2C%2F%25'
+
+    assert parse_qs( 'foo=%C3%A9&bar=qux+%2C%2F%25') == dict([(u'foo', [u'é']), (u'bar', [u'qux ,/%'])])
+    assert parse_qs(u'foo=%C3%A9&bar=qux+%2C%2F%25') == dict([(u'foo', [u'é']), (u'bar', [u'qux ,/%'])])
+
+    assert parse_qsl( 'foo=%C3%A9&bar=qux+%2C%2F%25') == [(u'foo', u'é'), (u'bar', u'qux ,/%')]
+    assert parse_qsl(u'foo=%C3%A9&bar=qux+%2C%2F%25') == [(u'foo', u'é'), (u'bar', u'qux ,/%')]
