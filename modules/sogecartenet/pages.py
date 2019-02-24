fc7621de12700bbbac66134da0acc19e3493cbf7
Romain Bignon <romain@symlink.me>
2019-02-24 21:31:06 +0100

    sogecartenet: change license to LGPL

--- modules/sogecartenet/pages.py
+++ modules/sogecartenet/pages.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 
-# Copyright(C) 2015 Vincent Paredes
+# Copyright(C) 2015 Budget Insight
 #
-# This file is part of weboob.
+# This file is part of a weboob module.
 #
-# weboob is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
+# This weboob module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# weboob is distributed in the hope that it will be useful,
+# This weboob module is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
+# GNU Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU Affero General Public License
-# along with weboob. If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public License
+# along with this weboob module. If not, see <http://www.gnu.org/licenses/>.
 
 import requests
 
