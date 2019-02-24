d7320d8b1ebaf65a9d3fc52ef117d2e92bd2c729
Romain Bignon <romain@symlink.me>
2019-02-24 10:50:58 +0100

    isolate copyright notices on weboob modules

--- modules/dpd/test.py
+++ modules/dpd/test.py
@@ -2,20 +2,20 @@
 
 # Copyright(C) 2015      Matthieu Weber
 #
-# This file is part of weboob.
+# This file is part of a weboob module.
 #
-# weboob is free software: you can redistribute it and/or modify
+# This weboob module is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# weboob is distributed in the hope that it will be useful,
+# This weboob module is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with weboob. If not, see <http://www.gnu.org/licenses/>.
+# along with this weboob module. If not, see <http://www.gnu.org/licenses/>.
 
 
 from weboob.tools.test import BackendTest
