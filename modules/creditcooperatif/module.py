7eb7b0eb376ae73184f8a0df90ba29441b4c508e
Romain Bignon <romain@symlink.me>
2019-03-17 15:25:52 +0100

    bourso and creditcoop: change license

--- modules/creditcooperatif/module.py
+++ modules/creditcooperatif/module.py
@@ -5,16 +5,16 @@
 # This file is part of a weboob module.
 #
 # This weboob module is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
+# it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This weboob module is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
+# GNU Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU Affero General Public License
+# You should have received a copy of the GNU Lesser General Public License
 # along with this weboob module. If not, see <http://www.gnu.org/licenses/>.
 
 from weboob.capabilities.bank import CapBankTransferAddRecipient
@@ -34,7 +34,7 @@ class CreditCooperatifModule(AbstractModule, CapBankTransferAddRecipient, CapPro
     EMAIL = 'weboob@kevin.pouget.me'
     VERSION = '1.6'
     DESCRIPTION = u'Crédit Coopératif'
-    LICENSE = 'AGPLv3+'
+    LICENSE = 'LGPLv3+'
     auth_type = {'particular': "Interface Particuliers",
                  'weak' : "Code confidentiel (pro)",
                  'strong': "Sesame (pro)"}
