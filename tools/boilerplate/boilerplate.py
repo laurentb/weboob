e4397edfbc1848c41abbe2d97f875cb957376195
Vincent A <dev@indigo.re>
2019-09-09 23:58:43 +0200

    python3: fix some more shebangs

--- tools/boilerplate/boilerplate.py
+++ tools/boilerplate/boilerplate.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # Copyright(C) 2013-2019      Laurent Bachelier, Sébastien Jean
--- tools/boilerplate/boilerplate_data/recipes.py
+++ tools/boilerplate/boilerplate_data/recipes.py
@@ -1,4 +1,3 @@
-#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
 # Copyright(C) 2013-2019      Laurent Bachelier, Sébastien Jean
