15c72e16146c3a142dc940d7896c325a00a44212
Quentin Defenouillere <quentin.defenouillere@budget-insight.com>
2019-09-25 17:45:38 +0200

    [weboob/tools/capabilities/bank/test.py] Modified valuation test for valuations = 0.00
    Some accounts have investments with a valuation = 0, it should not be
    present in the alert is opposed to empty valuations like NotAvailable.

--- weboob/tools/capabilities/bank/test.py
+++ weboob/tools/capabilities/bank/test.py
@@ -153,7 +153,7 @@ class BankStandardTest(object):
 
     def check_investment(self, account, inv):
         self.assertTrue(inv.label, 'investment %r has no label' % inv)
-        self.assertTrue(inv.valuation, 'investment %r has no valuation' % inv)
+        self.assertFalse(empty(inv.valuation), 'investment %r has no valuation' % inv)
         if inv.code and inv.code != 'XX-liquidity':
             self.assertTrue(inv.code_type, 'investment %r has code but no code type' % inv)
         if inv.code_type == inv.CODE_TYPE_ISIN and inv.code and not inv.code.startswith('XX'):
