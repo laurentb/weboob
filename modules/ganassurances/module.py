91f1f22f1795c283f1361589cddd8f105d1f8748
Quentin Defenouillere <quentin.defenouillere@budget-insight.com>
2019-10-15 16:57:04 +0200

    [ganassurances] Make this module standalone with former code from groupama
    Groupama has shifted to the new website and is now a child of
    ganpatrimoine, however ganassurances (a child of groupama) is still
    using the old website, to I shifted all the code from groupama here so
    ganassurances can still work correctly on the old website.

--- modules/ganassurances/module.py
+++ modules/ganassurances/module.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright(C) 2012-2013 Romain Bignon
+# Copyright(C) 2012-2019 Romain Bignon
 #
 # This file is part of a weboob module.
 #
@@ -17,14 +17,13 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this weboob module. If not, see <http://www.gnu.org/licenses/>.
 
+
 from __future__ import unicode_literals
 
-
-from collections import OrderedDict
-
-from weboob.capabilities.bank import CapBank
-from weboob.tools.backend import AbstractModule, BackendConfig
-from weboob.tools.value import ValueBackendPassword, Value
+from weboob.capabilities.bank import CapBankWealth, AccountNotFound
+from weboob.capabilities.base import find_object
+from weboob.tools.backend import Module, BackendConfig
+from weboob.tools.value import Value, ValueBackendPassword
 
 from .browser import GanAssurancesBrowser
 
@@ -32,31 +31,36 @@ from .browser import GanAssurancesBrowser
 __all__ = ['GanAssurancesModule']
 
 
-class GanAssurancesModule(AbstractModule, CapBank):
+class GanAssurancesModule(Module, CapBankWealth):
     NAME = 'ganassurances'
     MAINTAINER = 'Romain Bignon'
     EMAIL = 'romain@weboob.org'
     VERSION = '1.6'
     DESCRIPTION = 'Gan Assurances'
     LICENSE = 'LGPLv3+'
-    website_choices = OrderedDict([(k, '%s (%s)' % (v, k)) for k, v in sorted({
-        'espaceclient.groupama.fr': 'Groupama Banque',
-        'espaceclient.ganassurances.fr': 'Gan Assurances',
-        'espaceclient.ganpatrimoine.fr': 'Gan Patrimoine',
-    }.items(), key=lambda k_v: (k_v[1], k_v[0]))])
     CONFIG = BackendConfig(
-        Value('website', label='Banque', choices=website_choices, default='espaceclient.ganassurances.fr'),
-        ValueBackendPassword('login', label='Identifiant / N° Client ou Email ou Mobile', masked=False),
-        ValueBackendPassword('password', label='Mon mot de passe', regexp=r'^\d+$')
+        Value('login', label='Numéro client'),
+        ValueBackendPassword('password', label="Code d'accès")
     )
-
-    PARENT = 'groupama'
     BROWSER = GanAssurancesBrowser
 
     def create_default_browser(self):
         return self.create_browser(
-            self.config['website'].get(),
             self.config['login'].get(),
-            self.config['password'].get(),
-            weboob=self.weboob
+            self.config['password'].get()
         )
+
+    def iter_accounts(self):
+        return self.browser.get_accounts_list(need_iban=True)
+
+    def get_account(self, _id):
+        return find_object(self.browser.get_accounts_list(need_iban=True), id=_id, error=AccountNotFound)
+
+    def iter_history(self, account):
+        return self.browser.get_history(account)
+
+    def iter_coming(self, account):
+        return self.browser.get_coming(account)
+
+    def iter_investment(self, account):
+        return self.browser.get_investment(account)
