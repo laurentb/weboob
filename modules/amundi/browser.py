716ce660e9f9bf6a4a43cce6ff7b72c2c5eadcc7
Quentin Defenouillere <quentin.defenouillere@budget-insight.com>
2019-10-31 14:43:20 +0100

    [amundi] Implemented asset category & recommended period
    There are a lot of various investment pages depending on where the
    support comes from. I handled as many pages as I could find, if there
    are still some missing the module shouldn't crash and raise a warning so
    we can handle them later.

--- modules/amundi/browser.py
+++ modules/amundi/browser.py
@@ -17,11 +17,19 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this weboob module. If not, see <http://www.gnu.org/licenses/>.
 
-from .pages import LoginPage, AccountsPage, AccountHistoryPage
+from __future__ import unicode_literals
+
 from weboob.browser import URL, LoginBrowser, need_login
 from weboob.exceptions import BrowserIncorrectPassword
-from weboob.browser.exceptions import ClientError
-from weboob.capabilities.base import empty
+from weboob.browser.exceptions import ClientError, ServerError
+from weboob.capabilities.base import empty, NotAvailable
+
+from .pages import (
+    LoginPage, AccountsPage, AccountHistoryPage, AmundiInvestmentsPage, AllianzInvestmentPage,
+    EEInvestmentPage, EEInvestmentDetailPage, EEProductInvestmentPage, EresInvestmentPage,
+    CprInvestmentPage, BNPInvestmentPage, BNPInvestmentApiPage, AxaInvestmentPage,
+    EpsensInvestmentPage, EcofiInvestmentPage,
+)
 
 
 class AmundiBrowser(LoginBrowser):
@@ -31,6 +39,29 @@ class AmundiBrowser(LoginBrowser):
     authorize = URL(r'authorize', LoginPage)
     accounts = URL(r'api/individu/positionFonds\?flagUrlFicheFonds=true&inclurePositionVide=false', AccountsPage)
     account_history = URL(r'api/individu/operations\?valeurExterne=false&filtreStatutModeExclusion=false&statut=CPTA', AccountHistoryPage)
+
+    # Amundi.fr investments
+    amundi_investments = URL(r'https://www.amundi.fr/fr_part/product/view', AmundiInvestmentsPage)
+    # EEAmundi browser investments
+    ee_investments = URL(r'https://www.amundi-ee.com/part/home_fp&partner=PACTEO_SYS', EEInvestmentPage)
+    ee_investment_details = URL(r'https://www.amundi-ee.com/psAmundiEEPart/ezjscore/call', EEInvestmentDetailPage)
+    # EEAmundi product investments
+    ee_product_investments = URL(r'https://www.amundi-ee.com/product', EEProductInvestmentPage)
+    # Allianz GI investments
+    allianz_investments = URL(r'https://fr.allianzgi.com', AllianzInvestmentPage)
+    # Eres investments
+    eres_investments = URL(r'https://www.eres-group.com/eres/new_fiche_fonds.php', EresInvestmentPage)
+    # CPR asset management investments
+    cpr_investments = URL(r'https://www.cpr-am.fr/particuliers/product/view', CprInvestmentPage)
+    # BNP Paribas Epargne Retraite Entreprises
+    bnp_investments = URL(r'https://www.epargne-retraite-entreprises.bnpparibas.com/entreprises/fonds', BNPInvestmentPage)
+    bnp_investment_api = URL(r'https://www.epargne-retraite-entreprises.bnpparibas.com/api2/funds/overview/(?P<fund_id>.*)', BNPInvestmentApiPage)
+    # AXA investments
+    axa_investments = URL(r'https://(.*).axa-im.fr/fr/fund-page', AxaInvestmentPage)
+    # Epsens investments
+    epsens_investments = URL(r'https://www.epsens.com/information-financiere', EpsensInvestmentPage)
+    # Ecofi investments
+    ecofi_investments = URL(r'http://www.ecofi.fr/fr/fonds/dynamis-solidaire', EcofiInvestmentPage)
 
     def do_login(self):
         data = {
@@ -61,8 +92,88 @@ class AmundiBrowser(LoginBrowser):
             return
         headers = {'X-noee-authorization': 'noeprd %s' % self.token}
         self.accounts.go(headers=headers)
+
+        ignored_urls = (
+            'www.sggestion-ede.com/product',  # Going there leads to a 404
+            'www.assetmanagement.hsbc.com',  # Information not accessible
+            'www.labanquepostale-am.fr/nos-fonds',  # Nothing interesting there
+        )
+
+        handled_urls = (
+            'www.amundi.fr/fr_part',  # AmundiInvestmentsPage
+            'www.amundi-ee.com/part/home_fp',  # EEInvestmentPage
+            'www.amundi-ee.com/product',  # EEProductInvestmentPage
+            'fr.allianzgi.com/fr-fr',  # AllianzInvestmentPage
+            'www.eres-group.com/eres',  # EresInvestmentPage
+            'www.cpr-am.fr/particuliers/product',  # CprInvestmentPage
+            'www.epargne-retraite-entreprises.bnpparibas.com',  # BNPInvestmentPage
+            'axa-im.fr/fr/fund-page',  # AxaInvestmentPage
+            'www.epsens.com/information-financiere',  # EpsensInvestmentPage
+            'www.ecofi.fr/fr/fonds/dynamis-solidaire',  # EcofiInvestmentPage
+        )
+
         for inv in self.page.iter_investments(account_id=account.id):
+            if inv._details_url:
+                # Only go to known details pages to avoid logout on unhandled pages
+                if any(url in inv._details_url for url in handled_urls):
+                    self.fill_investment_details(inv)
+                else:
+                    if not any(url in inv._details_url for url in ignored_urls):
+                        # Not need to raise warning if the URL is already known and ignored
+                        self.logger.warning('Investment details on URL %s are not handled yet.', inv._details_url)
+                    inv.asset_category = NotAvailable
+                    inv.recommended_period = NotAvailable
             yield inv
+
+    @need_login
+    def fill_investment_details(self, inv):
+        # Going to investment details may lead to various websites.
+        # This method handles all the already encountered pages.
+        try:
+            self.location(inv._details_url)
+        except ServerError:
+            # Some URLs return a 500 even on the website
+            inv.asset_category = NotAvailable
+            inv.recommended_period = NotAvailable
+            return inv
+
+        # Pages with only asset category available
+        if (self.amundi_investments.is_here() or
+            self.allianz_investments.is_here() or
+            self.axa_investments.is_here()):
+            inv.asset_category = self.page.get_asset_category()
+            inv.recommended_period = NotAvailable
+
+        # Pages with asset category & recommended period
+        elif (self.eres_investments.is_here() or
+            self.cpr_investments.is_here() or
+            self.ee_product_investments.is_here() or
+            self.epsens_investments.is_here() or
+            self.ecofi_investments.is_here()):
+            self.page.fill_investment(obj=inv)
+
+        # Particular cases
+        elif self.ee_investments.is_here():
+            inv.recommended_period = self.page.get_recommended_period()
+            details_url = self.page.get_details_url()
+            if details_url:
+                self.location(details_url)
+                if self.ee_investment_details.is_here():
+                    inv.asset_category = self.page.get_asset_category()
+
+        elif self.bnp_investments.is_here():
+            # We fetch the fund ID and get the attributes directly from the BNP-ERE API
+            fund_id = self.page.get_fund_id()
+            if fund_id:
+                # Specify the 'Accept' header otherwise the server returns WSDL instead of JSON
+                self.bnp_investment_api.go(fund_id=fund_id, headers={'Accept': 'application/json'})
+                self.page.fill_investment(obj=inv)
+            else:
+                self.logger.warning('Could not fetch the fund_id for BNP investment %s.', inv.label)
+                inv.asset_category = NotAvailable
+                inv.recommended_period = NotAvailable
+
+        return inv
 
     @need_login
     def iter_history(self, account):
@@ -73,8 +184,10 @@ class AmundiBrowser(LoginBrowser):
 
 
 class EEAmundi(AmundiBrowser):
+    # Careful if you modify the BASEURL, also verify Amundi's Abstract modules
     BASEURL = 'https://www.amundi-ee.com/psf/'
 
 
 class TCAmundi(AmundiBrowser):
+    # Careful if you modify the BASEURL, also verify Amundi's Abstract modules
     BASEURL = 'https://epargnants.amundi-tc.com/psf/'
--- modules/amundi/pages.py
+++ modules/amundi/pages.py
@@ -19,18 +19,21 @@
 
 from __future__ import unicode_literals
 
+import re
 from datetime import datetime
 
 from weboob.browser.elements import ItemElement, method, DictElement
 from weboob.browser.filters.standard import (
-    CleanDecimal, Date, Field, CleanText, Env, Eval,
+    CleanDecimal, Date, Field, CleanText,
+    Env, Eval, Map, Regexp, Title,
 )
+from weboob.browser.filters.html import Attr
 from weboob.browser.filters.json import Dict
-from weboob.browser.pages import LoggedPage, JsonPage
+from weboob.browser.pages import LoggedPage, JsonPage, HTMLPage
 from weboob.capabilities.bank import Account, Investment, Transaction
-from weboob.capabilities.base import NotAvailable, empty
+from weboob.capabilities.base import NotAvailable
 from weboob.exceptions import NoAccountsException
-from weboob.tools.capabilities.bank.investments import is_isin_valid
+from weboob.tools.capabilities.bank.investments import IsinCode, IsinType
 
 
 class LoginPage(JsonPage):
@@ -38,20 +41,20 @@ class LoginPage(JsonPage):
         return Dict('token')(self.doc)
 
 
+ACCOUNT_TYPES = {
+    'PEE': Account.TYPE_PEE,
+    'PEG': Account.TYPE_PEE,
+    'PEI': Account.TYPE_PEE,
+    'PERCO': Account.TYPE_PERCO,
+    'RSP': Account.TYPE_RSP,
+}
+
 class AccountsPage(LoggedPage, JsonPage):
     def get_company_name(self):
         json_list = Dict('listPositionsSalarieFondsDto')(self.doc)
         if json_list:
             return json_list[0].get('nomEntreprise', NotAvailable)
         return NotAvailable
-
-    ACCOUNT_TYPES = {
-        'PEE': Account.TYPE_PEE,
-        'PEG': Account.TYPE_PEE,
-        'PEI': Account.TYPE_PEE,
-        'PERCO': Account.TYPE_PERCO,
-        'RSP': Account.TYPE_RSP,
-    }
 
     @method
     class iter_accounts(DictElement):
@@ -72,9 +75,7 @@ class AccountsPage(LoggedPage, JsonPage):
                 return '%s_%s' % (Field('id')(self), self.page.browser.username)
 
             obj_currency = 'EUR'
-
-            def obj_type(self):
-                return self.page.ACCOUNT_TYPES.get(Dict('typeDispositif')(self), Account.TYPE_LIFE_INSURANCE)
+            obj_type = Map(Dict('typeDispositif'), ACCOUNT_TYPES, Account.TYPE_LIFE_INSURANCE)
 
             def obj_label(self):
                 try:
@@ -108,6 +109,9 @@ class AccountsPage(LoggedPage, JsonPage):
             obj_quantity = Dict('nbParts') & CleanDecimal
             obj_valuation = Dict('mtBrut') & CleanDecimal
             obj_vdate = Date(Dict('dtVl'))
+            obj__details_url = Dict('urlFicheFonds', default=None)
+            obj_code = IsinCode(Dict('codeIsin', default=NotAvailable), default=NotAvailable)
+            obj_code_type = IsinType(Dict('codeIsin', default=NotAvailable))
 
             def obj_srri(self):
                 srri = Dict('SRRI')(self)
@@ -116,25 +120,14 @@ class AccountsPage(LoggedPage, JsonPage):
                     return NotAvailable
                 return int(srri)
 
-            def obj_code(self):
-                code = Dict('codeIsin', default=NotAvailable)(self)
-                if is_isin_valid(code):
-                    return code
-                return NotAvailable
-
-            def obj_code_type(self):
-                if empty(Field('code')(self)):
-                    return NotAvailable
-                return Investment.CODE_TYPE_ISIN
-
             def obj_performance_history(self):
                 # The Amundi JSON only contains 1 year and 5 years performances.
                 # It seems that when a value is unavailable, they display '0.0' instead...
                 perfs = {}
                 if Dict('performanceUnAn', default=None)(self) not in (0.0, None):
-                    perfs[1] = Eval(lambda x: x/100, CleanDecimal(Dict('performanceUnAn')))(self)
+                    perfs[1] = Eval(lambda x: x / 100, CleanDecimal(Dict('performanceUnAn')))(self)
                 if Dict('performanceCinqAns', default=None)(self) not in (0.0, None):
-                    perfs[5] = Eval(lambda x: x/100, CleanDecimal(Dict('performanceCinqAns')))(self)
+                    perfs[5] = Eval(lambda x: x / 100, CleanDecimal(Dict('performanceCinqAns')))(self)
                 return perfs
 
 
@@ -170,5 +163,111 @@ class AccountHistoryPage(LoggedPage, JsonPage):
                     tr.date = tr.rdate
                     tr.label = hist.get('libelleOperation') or hist['libelleCommunication']
                     tr.type = Transaction.TYPE_UNKNOWN
-
                     yield tr
+
+
+class AmundiInvestmentsPage(LoggedPage, HTMLPage):
+    def get_asset_category(self):
+        # Descriptions are like 'Fonds d'Investissement - (ISIN: FR001018 - Action'
+        # Fetch the last words of the description (e.g. 'Action' or 'Diversifié')
+        return Regexp(
+            CleanText('//div[@class="amundi-fund-legend"]//strong'),
+            r' ([^-]+)$',
+            default=NotAvailable
+        )(self.doc)
+
+
+class EEInvestmentPage(LoggedPage, HTMLPage):
+    def get_recommended_period(self):
+        return Title(CleanText('//label[contains(text(), "Durée minimum de placement")]/following-sibling::span', default=NotAvailable))(self.doc)
+
+    def get_details_url(self):
+        return Attr('//a[contains(text(), "Caractéristiques")]', 'data-href', default=None)(self.doc)
+
+
+class EEInvestmentDetailPage(LoggedPage, HTMLPage):
+    def get_asset_category(self):
+        return CleanText('//label[contains(text(), "Classe d\'actifs")]/following-sibling::span', default=NotAvailable)(self.doc)
+
+
+class EEProductInvestmentPage(LoggedPage, HTMLPage):
+    @method
+    class fill_investment(ItemElement):
+        obj_asset_category = CleanText('//span[contains(text(), "Classe")]/following-sibling::span[@class="valeur"][1]')
+        obj_recommended_period = CleanText('//span[contains(text(), "Durée minimum")]/following-sibling::span[@class="valeur"][1]')
+
+
+class AllianzInvestmentPage(LoggedPage, HTMLPage):
+    def get_asset_category(self):
+        # The format may be a very short description, or be
+        # included between quotation marks within a paragraph
+        asset_category = CleanText('//div[contains(@class, "fund-summary")]//h3/following-sibling::div', default=NotAvailable)(self.doc)
+        m = re.search(r'« (.*) »', asset_category)
+        if m:
+            return m.group(1)
+        return asset_category
+
+
+class EresInvestmentPage(LoggedPage, HTMLPage):
+    @method
+    class fill_investment(ItemElement):
+        obj_asset_category = CleanText('//li[span[contains(text(), "Classification")]]', children=False, default=NotAvailable)
+        obj_recommended_period = CleanText('//li[span[contains(text(), "Durée")]]', children=False, default=NotAvailable)
+
+        def obj_performance_history(self):
+            perfs = {}
+            if CleanDecimal.French('(//tr[th[text()="1 an"]]/td[1])[1]', default=None)(self):
+                perfs[1] = Eval(lambda x: x / 100, CleanDecimal.French('(//tr[th[text()="1 an"]]/td[1])[1]'))(self)
+            if CleanDecimal.French('(//tr[th[text()="3 ans"]]/td[1])[1]', default=None)(self):
+                perfs[3] = Eval(lambda x: x / 100, CleanDecimal.French('(//tr[th[text()="3 ans"]]/td[1])[1]'))(self)
+            if CleanDecimal.French('(//tr[th[text()="5 ans"]]/td[1])[1]', default=None)(self):
+                perfs[5] = Eval(lambda x: x / 100, CleanDecimal.French('(//tr[th[text()="5 ans"]]/td[1])[1]'))(self)
+            return perfs
+
+
+class CprInvestmentPage(LoggedPage, HTMLPage):
+    @method
+    class fill_investment(ItemElement):
+        obj_srri = CleanText('//span[@class="active"]', default=NotAvailable)
+        obj_asset_category = CleanText('//div[contains(text(), "Classe d\'actifs")]//strong', default=NotAvailable)
+        obj_recommended_period = CleanText('//div[contains(text(), "Durée recommandée")]//strong', default=NotAvailable)
+
+
+class BNPInvestmentPage(LoggedPage, HTMLPage):
+    def get_fund_id(self):
+        return Regexp(
+            CleanText('//script[contains(text(), "GLB_ProductId")]'),
+            r'GLB_ProductId = "(\w+)',
+            default=None
+        )(self.doc)
+
+
+class BNPInvestmentApiPage(LoggedPage, JsonPage):
+    @method
+    class fill_investment(ItemElement):
+        obj_asset_category = Dict('Classification', default=NotAvailable)
+        obj_recommended_period = Dict('DureePlacement', default=NotAvailable)
+
+
+class AxaInvestmentPage(LoggedPage, HTMLPage):
+    def get_asset_category(self):
+        return Title(CleanText('//th[contains(text(), "Classe")]/following-sibling::td'))(self.doc)
+
+
+class EpsensInvestmentPage(LoggedPage, HTMLPage):
+    @method
+    class fill_investment(ItemElement):
+        obj_asset_category = CleanText('//div[div[span[contains(text(), "Classification")]]]/div[2]/span', default=NotAvailable)
+        obj_recommended_period = CleanText('//div[div[span[contains(text(), "Durée de placement")]]]/div[2]/span', default=NotAvailable)
+
+
+class EcofiInvestmentPage(LoggedPage, HTMLPage):
+    @method
+    class fill_investment(ItemElement):
+        # Recommended period is actually an image so we extract the
+        # information from its URL such as '/Horizon/Horizon_5_ans.png'
+        obj_recommended_period = Regexp(
+            CleanText(Attr('//img[contains(@src, "/Horizon/")]', 'src', default=NotAvailable), replace=[(u'_', ' ')]),
+            r'\/Horizon (.*)\.png'
+        )
+        obj_asset_category = CleanText('//div[contains(text(), "Classification")]/following-sibling::div[1]', default=NotAvailable)
