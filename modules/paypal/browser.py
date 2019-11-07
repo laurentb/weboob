35374deb57e5281e3bfe5284fe1ad8ac1c9cc123
nicofrand <mail@nicofrand.eu>
2019-11-07 22:01:33 +0100

    [paypal] Fix get_accounts

--- modules/paypal/browser.py
+++ modules/paypal/browser.py
@@ -64,8 +64,8 @@ class Paypal(LoginBrowser):
                           HistoryDetailsPage)
     promo = URL('https://www.paypal.com/fr/webapps/mpp/clickthru/paypal-app-promo-2.*',
                 '/fr/webapps/mpp/clickthru.*', PromoPage)
-    account = URL('https://www.paypal.com/businessexp/money',
-                  'https://www.paypal.com/myaccount/money',
+    account = URL('https://www.paypal.com/myaccount/money',
+                  'https://www.paypal.com/businessexp/money',
                   'https://www.paypal.com/webapps/business/money', AccountPage)
     pro_history = URL('https://\w+.paypal.com/businessexp/transactions/activity\?.*',
                       ProHistoryPage)
