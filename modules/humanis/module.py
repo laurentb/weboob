24dc8abf365fb3d11d81befbf72ae591e75c3387
Quentin Defenouillere <quentin.defenouillere@budget-insight.com>
2019-11-20 17:18:58 +0100

    [humanis] Cosmetic & contributor changes in module.py

--- modules/humanis/module.py
+++ modules/humanis/module.py
@@ -17,11 +17,11 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this weboob module. If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import unicode_literals
 
 from weboob.tools.backend import Module, BackendConfig
 from weboob.tools.value import ValueBackendPassword
-from weboob.capabilities.bank import CapBankPockets, AccountNotFound
-from weboob.capabilities.base import find_object
+from weboob.capabilities.bank import CapBankPockets
 
 from .browser import HumanisBrowser
 
@@ -31,13 +31,15 @@ __all__ = ['HumanisModule']
 
 class HumanisModule(Module, CapBankPockets):
     NAME = 'humanis'
-    DESCRIPTION = u'Humanis Épargne Salariale'
-    MAINTAINER = u'Jean Walrave'
-    EMAIL = 'jwalrave@budget-insight.com'
+    DESCRIPTION = 'Humanis Épargne Salariale'
+    MAINTAINER = 'Quentin Defenouillère'
+    EMAIL = 'quentin.defenouillere@budget-insight.com'
     LICENSE = 'LGPLv3+'
     VERSION = '1.6'
-    CONFIG = BackendConfig(ValueBackendPassword('login',    label=u'Code d\'accès', masked=False),
-                           ValueBackendPassword('password', label='Mot de passe'))
+    CONFIG = BackendConfig(
+        ValueBackendPassword('login', label='Code d\'accès', masked=False),
+        ValueBackendPassword('password', label='Mot de passe')
+    )
 
     BROWSER = HumanisBrowser
 
@@ -49,9 +51,6 @@ class HumanisModule(Module, CapBankPockets):
             'epsens/',
             weboob=self.weboob
         )
-
-    def get_account(self, _id):
-        return find_object(self.browser.iter_accounts(), id=_id, error=AccountNotFound)
 
     def iter_accounts(self):
         return self.browser.iter_accounts()
