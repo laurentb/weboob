406d90cf52ecd5051bcd92fe10dfafa8c097a11c
Guillaume Risbourg <guillaume.risbourg@budget-insight.com>
2020-01-27 10:27:00 +0100

    [carrefourbanque] Add missing unicode_literals import
    Without this import, the check on wrongpass was not working because of
    special char in the string.
    
    Closes: 56235@sibi

--- modules/carrefourbanque/browser.py
+++ modules/carrefourbanque/browser.py
@@ -16,6 +16,9 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this weboob module. If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import absolute_import, unicode_literals
+
 from time import sleep
 
 from weboob.browser import LoginBrowser, URL, need_login, StatesMixin
