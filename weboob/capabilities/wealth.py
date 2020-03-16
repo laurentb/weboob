64e08ed2b07615178e08a41a2a93465bdfab745c
Quentin Defenouillere <quentin.defenouillere@budget-insight.com>
2020-03-16 18:38:26 +0100

    [capabilities/wealth] Create file wealth.py with temporary imports
    These imports will be used while we shift all module imports to
    weboob.capabilities.wealth, we all is done we can move the
    wealth-related classes from bank.py to wealth.py
    The "__all__" makes sure that classes are used to avoid pyflakes crashes
    'import but unused'.

---
+++ weboob/capabilities/wealth.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2020      Quentin Defenouillere
+#
+# This file is part of weboob.
+#
+# weboob is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# weboob is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with weboob. If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+# Temporary imports before moving these classes in this file
+from weboob.capabilities.bank import (
+    PerVersion, PerProviderType, Per, Investment, Pocket,
+    MarketOrderType, MarketOrderDirection, MarketOrder,
+    CapBankWealth,
+)
+
+__all__ = [
+    'PerVersion', 'PerProviderType', 'Per', 'Investment', 'Pocket',
+    'MarketOrderType', 'MarketOrderDirection', 'MarketOrder',
+    'CapBankWealth',
+]
